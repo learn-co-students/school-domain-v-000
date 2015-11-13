e43b1eb2020289f9b7dc60b95591d06f0b9dc3ad
github username <github email address>
2015-11-13 16:12:51 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School 
+  attr_accessor :name, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {} 
+  end
+
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_students = {}
+    @roster.map do |grade, students | 
+      sorted_students[grade] = students.sort
+    end
+    sorted_students
+  end
+end
