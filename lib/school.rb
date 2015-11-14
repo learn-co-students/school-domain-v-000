8a68b0966bec27e2c2f281f8c81f6635be082115
Ahertig <alyssa.hertig@gmail.com>
2015-11-14 12:09:54 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+  def initialize(name)
+    @school = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if @roster[grade] == nil
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade_hash, name_arr|
+      @roster[grade_hash]= name_arr.sort
+    end
+
+    @roster
+  end
+
+end
+
