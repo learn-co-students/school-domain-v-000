726f321160087c2f969f4da68f0070ae9364436d
Justin Thomas <jthomas0209@gmail.com>
2015-11-18 14:17:33 -0500

    Created class School

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  def initialize(school)
+    @name = school
+    @roster = {}
+  end
+
+  attr_accessor :name
+  attr_reader :roster
+
+  def add_student(name,grade)
+    @roster[grade] == nil ? roster[grade] = [name] : roster[grade] << name
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade,students|
+      students.sort!
+    end
+  end
+end
