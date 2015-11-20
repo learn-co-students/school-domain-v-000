fc65add9291c9600885a22096dba1151042a5999
surla <josephsurla@gmail.com>
2015-11-19 20:22:25 -0800

    created school class

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+   attr_accessor :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+      roster[grade] = [] unless roster.include?(grade)
+      roster[grade] << name
+  end
+
+  def grade(number)
+    roster[number]
+  end
+
+  def sort
+    sort = {}
+    roster.each do |grade, name|
+      sort[grade] = name.sort
+    end
+    sort
+  end
+end
