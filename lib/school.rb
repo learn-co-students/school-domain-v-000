8bb2f8f291f4c284a8579764835589766e1332bf
lisamarie616 <lisamarie616@gmail.com>
2015-12-01 17:34:16 -0800

    correct roster variable in all methods

--- lib/school.rb
+++ lib/school.rb
@@ -9,17 +9,17 @@ class School
   end
 
   def add_student(student_name, student_grade)
-    @roster[student_grade] = [] if @roster[student_grade] == nil
-    @roster[student_grade] << student_name
+    roster[student_grade] ||= [] #if @roster[student_grade] == nil
+    roster[student_grade] << student_name
   end
 
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
 
   def sort
-    @roster.each do |student_grade, student_name|
-      @roster[student_grade].sort!
+    roster.each do |student_grade, student_name|
+      roster[student_grade].sort!
     end
   end
 
