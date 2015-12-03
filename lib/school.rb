b993dff2ed528b43f8abedd2d2511eeec56b2336
authorbeard <msnolan@gmail.com>
2015-12-03 12:13:43 -0500

    All tests passing

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+
+class School
+  # attr_accessor :student, :grade
+  attr_reader :roster
+
+  def initialize(roster)
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_students = {}
+    @roster.each {|grade, students|
+      sorted_students[grade] = students.sort!
+    }
+  end
+
+end
