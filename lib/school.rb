e6c9d09d127fc91f4510d9b3adc4e8b64daa48f3
mmmeyers <marsi286@gmail.com>
2015-12-03 18:23:33 -0500

    Cleaned up original code

--- lib/school.rb
+++ lib/school.rb
@@ -1,39 +1,26 @@
+require 'pry'
+
 class School
 
-  attr_reader :roster
+  attr_accessor :name, :roster
 
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-  def name
-    @name
-  end
-
-  def roster=(roster)
-    @roster = roster
-    ROSTER << @roster
-  end
-
-
   def add_student(name, grade)
-    if @roster.empty?
-      @roster[grade] = []
-      @roster[grade] << name
-    elsif @roster.keys.include?(grade)
-      @roster[grade] << name
-    elsif @roster.keys.include?(grade) == false
-      @roster[grade] = [name]
-    end 
+    roster[grade] ||= []
+    roster[grade] << name
   end
 
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
 
   def sort
-    @roster.each {|grade, name| name.sort!}
-      @roster.to_h
-    end
+    roster.each {|grade, name| name.sort!}
+    roster
   end
+
+end
