a245dffa62a558bfedf671c93423e7ecba4b7ec4
yoshikot <ytamaoki@gmail.com>
2015-12-10 17:42:18 -0500

    some code that doesn't work

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  # attr accessor :roster
+  attr_reader :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = []
+  end
+
+  def add_student(student, placement=0)
+    @roster[placement-1] = student
+    print @roster
+  end
+
+  def roster()
+
+end
+
+Yo = School.new("Yo")
+Yo.add_student("AC", 10) 
