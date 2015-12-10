0477e20bdcaa0616f1f09c3dfb91e2fbd3d2c4a1
Dakota Martinez <dakotaleemusic@gmail.com>
2015-12-10 00:45:56 +0000

    completes school domain lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require 'pry'
+
+class School
+    attr_accessor :roster
+    
+    def initialize(name)
+       @name = name
+       @roster = {}
+    end
+    
+    def add_student(name, grade)
+       @roster[grade] = [] if @roster.keys.none? {|g| g == grade}
+       @roster[grade] << name
+    end
+    
+    def grade(num)
+        @roster[num]
+    end
+    
+    def sort
+        Hash[ @roster.map { |grade, students| [grade, students.sort] } ]
+    end
+    
+end
