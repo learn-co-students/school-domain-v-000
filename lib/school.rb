9990a3c99e728fb73ecd3addb835f12a9be75cd3
yoshikot <ytamaoki@gmail.com>
2015-12-10 19:25:58 -0500

    looked into OR Equal syntax and implemented

--- lib/school.rb
+++ lib/school.rb
@@ -7,22 +7,16 @@ class School
   end
 
   def add_student(student, input_grade)
-    if @roster.keys.include?(input_grade)
-      @roster.each do |grade, students|
-        if grade == input_grade
-          students << student
-        end
-      end
-    else @roster[input_grade] = [student]
-    end
+    roster[input_grade] ||= []
+    roster[input_grade] << student
   end
 
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
 
   def sort
-    @roster.each do |grade, students|
+    roster.each do |grade, students|
       students.sort!
     end
   end
