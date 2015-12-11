a0b97222470fdda6496769bbdad320620c5fe3cb
yoshikot <ytamaoki@gmail.com>
2015-12-10 19:15:05 -0500

    done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,30 @@
 class School
-  # attr accessor :roster
-  attr_reader :school, :roster
+  attr_reader :name, :roster
 
   def initialize(school)
-    @school = school
-    @roster = []
+    @name = school
+    @roster = {}
   end
 
-  def add_student(student, placement=0)
-    @roster[placement-1] = student
-    print @roster
+  def add_student(student, input_grade)
+    if @roster.keys.include?(input_grade)
+      @roster.each do |grade, students|
+        if grade == input_grade
+          students << student
+        end
+      end
+    else @roster[input_grade] = [student]
+    end
   end
 
-  def roster()
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
 
 end
-
-Yo = School.new("Yo")
-Yo.add_student("AC", 10) 
