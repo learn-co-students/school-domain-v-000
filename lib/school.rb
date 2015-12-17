443b597fa2fd1f218415243d99eaeb2c27554252
febbraiod <donedesign1@gmail.com>
2015-12-17 15:34:09 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -27,7 +27,6 @@ class School
 
 
   def sort
-    #sorted_hash = {}
 
     @roster.each do |grade, names|
       @roster[grade] = names.sort
@@ -36,6 +35,4 @@ class School
     roster
   end
 
-
-
-end
+end #end of class
