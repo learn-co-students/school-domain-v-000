1e257922a02ae7014808c9f60d19430241d8bba3
febbraiod <donedesign1@gmail.com>
2015-12-17 15:33:32 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name
+    @roster = {}
+  end
+
+
+  def roster
+    @roster
+  end
+
+
+  def add_student(name, grade)
+    if roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+
+  def grade(num)
+    @roster[num]
+  end
+
+
+  def sort
+    #sorted_hash = {}
+
+    @roster.each do |grade, names|
+      @roster[grade] = names.sort
+    end
+
+    roster
+  end
+
+
+
+end
