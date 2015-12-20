b9bac52dd7f8f779a977bb43284fd23709fdc164
preetdha <manpreet.dha@gmail.com>
2015-12-19 22:59:21 -0800

    used class and methods to make a domain model

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+  attr_accessor :roster, :school
+  
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    #roster[grade] = []
+    if roster.include?(grade)  
+      roster[grade] << student_name 
+    else
+      roster[grade] = []
+      roster[grade] << student_name
+    end
+  end
+
+  def grade(school_grade)
+    roster[school_grade]
+  end
+
+  def sort
+    sorted_students = {}
+    roster.each do |grade, name|
+     sorted_students[grade] = name.sort    
+    end
+    sorted_students
+  end
+end
