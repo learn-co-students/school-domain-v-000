67388adaac70f6c8081acef7902fbbdd3dfcece3
Tsundu <osmentdan@gmail.com>
2015-12-21 07:09:59 -0500

    school-domain built

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
-# code here!
+class School 
+  def initialize(name)
+    @name = name 
+    
+    @roster = {}
+  end 
+
+  attr_reader :name, :roster
+
+  def add_student(name, grade)
+    
+    @roster[grade] ||= []
+
+    @roster[grade]<<name
+
+  end 
+
+  def grade(grade)
+    @roster[grade]
+
+  end 
+
+  def sort 
+    @roster.collect do |key, value|
+       value.sort!
+    end
+    @roster  
+  end  
+
+end 
+
+
+
+
+
+
+
+
+
+
