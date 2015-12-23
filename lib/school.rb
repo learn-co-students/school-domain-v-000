a124b0dbb8ce0755cae2f5c12c6525cd3680347a
eric-an <erican@me.com>
2015-12-23 09:46:55 -0800

    create domain model w/ instance methods that manipulate nested hashes/arrays

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, level)
+    if @roster.include?(level)
+      @roster[level] << name
+    else
+      @roster[level] = [name]
+    end
+  end
+
+  def grade(level)
+    @roster[level]
+  end
+
+  def sort
+    @roster.map { |data, value| @roster[data] = value.sort }
+    @roster
+  end
+end
