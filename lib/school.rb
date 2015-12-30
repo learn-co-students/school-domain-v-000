5e8c4e0e726949a8405ca87c9e0eb18d9dc23691
Chris Bruen <bruen.chris@gmail.com>
2015-12-30 17:01:30 -0500

    one more thing

--- lib/school.rb
+++ lib/school.rb
@@ -8,9 +8,12 @@ class School
 		@roster = {}
 	end
 	def add_student(name, grade)
-		@roster[grade].nil? ? @roster[grade] = name : roster[grade] << name
+		@roster[grade].nil? ? @roster[grade] = [name] : roster[grade] << name
 	end
 	def grade(grade)
 		@roster[grade]
+	end
+	def sort
+		@roster.keys.each { |x| @roster[x] = @roster[x].sort }
 	end
 end
