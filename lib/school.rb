d0d53f5952c99f90eff443542886c345ab68baec
Chris Bruen <bruen.chris@gmail.com>
2015-12-30 16:18:42 -0500

    try

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
 # code here!
+class School
+	def initialize(school)
+		@school = school
+		@roster = Hash.new([])
+	end
+	def add_student(name, grade)
+		@roster[grade] << name
+	end
+	def grade(grade)
+		@roster[grade]
+	end
+end
