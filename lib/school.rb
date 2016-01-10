e994bc278379e20a1a10e880dc2af722fe6c6017
Brad1974 <danieltbrad@gmail.com>
2016-01-10 12:14:15 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+
+  attr_accessor :name, :roster
+
+  def add_student(student, grade)
+    if roster.include?(grade)
+      roster[grade] << student
+    else
+      roster[grade] = []
+      roster[grade] << student
+    end
+  end
+
+# roster = {"9" => ["Tim", "Jim"], "10" => ["Sally", "Harry"]}
+
+  def grade(grade)
+    roster[grade].map {|student| student}
+  end
+
+  def sort
+    roster.each {|grade, students| students.sort!}
+  end
+
+end
+
