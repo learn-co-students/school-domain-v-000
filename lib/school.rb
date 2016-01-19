842944f1c5ba2a3cbb1de290ab8208c06759428d
drywaterr24 <angelolea24@gmail.com>
2016-01-19 11:59:48 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+
+  def initialize(model)
+    @roster = {}
+    @model = model
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if !@roster.has_key?(grade)
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, names|
+      names.sort!
+    end
+    @roster
+  end
+
+
+end
