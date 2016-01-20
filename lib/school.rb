1e56c1a58c62a681609e7fc67a27cffb0c1802c8
krisnotchris <krishenderson@gmail.com>
2016-01-19 19:30:41 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name,grade)
+      roster[grade] ||=[]
+      roster[grade] << name
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    new_hash = Hash.new
+    roster.each do |key,value|
+      #binding.pry
+      new_hash[key] = value.sort
+    end
+    new_hash
+  end
+
+
+end
