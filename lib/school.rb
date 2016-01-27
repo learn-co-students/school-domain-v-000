de0e7cb4b8858331d6239d8f936ecb28ebc7a26d
Rafael A. Mejia <rafael@dataminded.net>
2016-01-27 03:17:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if roster[grade] == nil
+      roster[grade] = []
+    end
+    roster[grade] << name
+
+    return nil
+  end
+
+  def grade(grade)
+    return roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |key, value|
+      sorted[key] = value.sort
+    end
+    return sorted
+  end
+
+
+
+end
+
