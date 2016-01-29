d4d56efc55169ffdeb76272551b385b1743d5796
zachnewburgh <zach.newburgh@gmail.com>
2016-01-29 12:38:15 -0500

    created school domains with add_student, grade, and sort methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
+require 'pry'
+
 # code here!
+class School
+  attr_reader :name, :roster
+
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+  end
+
+  def add_student(name, grade)
+    if roster.include?(grade)
+      roster[grade] << name
+    else
+      roster.merge!(grade => [name])
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |grade, names|
+      sorted[grade] = names.sort
+    end
+    sorted
+  end
+
+end
