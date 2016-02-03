85918dcc33376924a12507dbb545c2cd48601f42
NStephenson <nickstephenson90@gmail.com>
2016-02-03 17:19:51 -0500

    made all tests pass

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+
+  attr_reader :school, :roster
+
+  def initialize(name)
+    @school = name
+    @roster = {}
+  end
+
+
+
+  def add_student(name, grade)
+    if roster.keys.include?(grade)
+      roster[grade] << name
+    else
+      roster[grade] = []
+      roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each do |grades, students|
+      students.sort!
+    end
+  end
+
+
+end
+
