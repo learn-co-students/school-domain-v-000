5f034258a4732dbb8abbc226dab0c531a4cc40be
lamass <hadzy@yahoo.com>
2016-02-03 21:18:08 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -7,12 +7,14 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster.include?(grade)  #if grade exists 
-      @roster[grade].push(name)  # push the student
-    else
-      @roster[grade] = []    # if grade does not exist associate with it an empty []
-      @roster[grade].push(name) # and push the name
-    end
+#     if @roster.include?(grade)  #if grade exists
+#       @roster[grade].push(name)  # push the student
+#     else
+#       @roster[grade] = []    # if grade does not exist associate with it an empty []
+#       @roster[grade].push(name) # and push the name
+#     end
+    @roster[grade] ||= [] #  or avoid the upper solution with this ...
+    @roster[grade] << name
   end
 
   def grade(grade)
