5b51e4a443e3b75da94a1ac6d4324412d46eefa5
lamass <hadzy@yahoo.com>
2016-02-03 21:08:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_reader :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.include?(grade)  #if grade exists 
+      @roster[grade].push(name)  # push the student
+    else
+      @roster[grade] = []    # if grade does not exist associate with it an empty []
+      @roster[grade].push(name) # and push the name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    ordered = {}
+    @roster.each do |grade, name|
+      ordered[grade] = name.sort
+    end
+    ordered
+  end
+end
+
