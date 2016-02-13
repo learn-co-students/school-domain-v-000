7395615f006ce0b15cd695c96e7c6fba57592c47
abadfish <kirsten.ofarrell@gmail.com>
2016-02-13 13:31:33 -0500

    worst lab ever

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each do |grades, names|
+      names.sort! 
+    end 
+  end
+end
