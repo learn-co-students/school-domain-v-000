773ef8332c542f1b1ac79699699f249c7555109a
bedwardsphoto <brian@brianedwardsphotography.com>
2016-02-14 16:40:26 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+attr_accessor :roster
+
+def initialize (school_name)
+  @school_name=school_name
+  @roster={}
+end
+def add_student(name,grade)
+  if @roster[grade]==nil
+    @roster[grade]=[]
+  end
+@roster[grade]<<name
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+ @roster.collect do |grade,students|
+   students.sort!
+  end
+  @roster
+end
+
+end
