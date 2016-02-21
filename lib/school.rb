4f4fc96bfc5fb3e2e4c24de0259d14b1b0bd1acb
the-widget <brennenawana108ny@gmail.com>
2016-02-21 09:46:19 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+  end
+
+  def add_student(student, num)
+    if roster.include?(num)
+      roster[num] << student
+    else
+      roster[num] = []
+      roster[num] << student
+    end
+  end
+
+  def grade(num)
+    roster[num]
+  end
+
+  def sort
+    roster.map{|key, value| value.sort!}
+    roster.sort_by {|k, v| k }.to_h
+  end
+  
+  
+end
