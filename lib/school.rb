fa76b79762430af6fede8ca38ffbf9b6b87d78bc
jbok4 <jbok4@aol.com>
2016-02-21 23:06:34 -0500

    first build, failing

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+
+  attr_accessor :grade
+  attr_reader :name
+
+  roster[:name] = []
+  roster[:name] << :grade
+
+  def initialize(name)
+    @aname = name
+  end
+
+end
