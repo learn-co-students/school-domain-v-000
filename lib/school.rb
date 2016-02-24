e60e1deb13942f5c9251eba9583cb604d8017c07
CodeByLine <yleventhal@ymail.com>
2016-02-24 17:20:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School
+
+  attr_accessor :school, :roster, :grade, :student, :students
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+
+  grade = {}
+
+  def new(roster)
+    @roster=roster
+  end
+
+  def add_student(student, grade)
+    if roster[grade].length == 0
+      roster[grade] = []
+      roster[grade] << student
+    else
+     roster[grade].push(student)
+    end
+  end
+
+  def grade(grade)
+    @grade = grade
+
+  end
+
+  def sort(students)
+  end
+
+end
