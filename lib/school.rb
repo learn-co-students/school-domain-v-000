5cb72c2eafb2b282d096c3899a43c5e860c0d9a3
CodeByLine <yleventhal@ymail.com>
2016-02-24 18:37:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,8 @@
 # code here!
+require 'pry'
 class School
 
-  attr_accessor :school, :roster, :grade, :student, :students
+  attr_accessor :school, :roster, :grade  #, :student, :students
 
   def initialize(school)
     @school = school
@@ -16,20 +17,50 @@ class School
   end
 
   def add_student(student, grade)
-    if roster[grade].length == 0
-      roster[grade] = []
+
+    roster[grade] ||= []
+    #if roster[grade].include?(student)
+#       roster[grade].push(student)
+#     else
+#       roster[grade] = []
       roster[grade] << student
-    else
-     roster[grade].push(student)
-    end
+   # end
+#        if roster[grade].length == 0
+#           roster[grade] = []
+#           roster[grade] << student
+#        else
+#           roster[grade].push(student)
+#        end
   end
 
   def grade(grade)
-    @grade = grade
+    roster[grade]
 
   end
 
-  def sort(students)
-  end
+  def sort
+    all_students = {}
+    roster.each do |grade, students| #@roster.collect: produces nested hash
+  #   binding.pry
+      all_students[grade] = students.sort
+      #  all_students << students
+      #  all_students= all_students.sort
+      end
+      return all_students
+    end
 
-end
+#binding.pry
+#   def sort
+#     all_students = []
+#     roster.each do |grade, students| #@roster.collect: produces nested hash
+#   #   binding.pry
+#       students.collect do |pupil|
+#        # binding.pry
+#         all_students << pupil
+#         all_students= all_students.sort
+#       end
+#       end
+#       return all_students
+#     end
+
+  end
