679e899fc4f028cd95a2dc439fe83af2cc3b88a0
tmharkins <tmharkins@gmail.com>
2016-02-24 02:26:24 -0600

    learned how to initialize and build hash with array value

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+class School
+
+	def initialize(school_name)
+		@roster = {}
+	end
+
+	def grade(grade)
+		@roster[grade]
+	end
+
+	def sort
+		@roster.each do |key, value|
+			value.sort!
+		end
+	end
+
+	def roster
+		@roster
+	end
+
+	def add_student(student_value, grade_key)
+		if @roster.keys.include?(grade_key)
+			@roster[grade_key] << student_value
+		else
+
+			@roster[grade_key] = [student_value]
+		end
+	end
+end
