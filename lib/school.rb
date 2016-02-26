cdee65d65c8ac7a28909c89635c709b258ae77be
cernanb <cernanb@gmail.com>
2016-02-25 18:27:12 -0800

    created School class with methods to pass all tests

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    if @roster[grade] == nil
+      @roster[grade] = Array.new << student
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
+
+end
+
