46487963d61759030a7c9118274a4521c81bc14c
maireadmurphy <mairead.aw.murphy@gmail.com>
2016-02-29 19:48:03 -0500

    created class of school with instance methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+def initialize(name)
+  @name = name
+  @roster = Hash.new
+end
+
+def roster
+  @roster
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def add_student(student, grade)
+  if @roster[grade] == nil
+    @roster[grade] = Array.new << student
+  else
+    @roster[grade] << student
+  end
+  end
+
+def sort
+  @roster.each do |grade, student|
+    student.sort!
+  end
+end
+
+end
