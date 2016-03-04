57468bcc2ad228c4c19a4c496573b56c3839a275
Kenneth Amper <kenn.amp@gmail.com>
2016-03-04 19:32:24 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+
+class School
+
+  attr_accessor :roster
+
+  def initialize(name)
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster.has_key?(grade) #not .defined? or .exists? lol
+      roster[grade] << student
+    else
+      roster[grade] = [student]
+    end
+
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.sort.to_h
+    temp = {}
+    @roster.each do |k,v|
+      temp[k] = v.sort
+    end
+
+     @roster = temp
+    #@roster.sort.to_h
+  end
+
+
+
+
+end
