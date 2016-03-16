53d4fe7828cfd7084abb1dae0910d40e95d92e76
SiamKing <thesiameseelectric@gmail.com>
2016-03-16 09:38:09 -0700

    schooled again

--- lib/school.rb
+++ lib/school.rb
@@ -8,10 +8,8 @@ class School
   end
 
   def add_student(student_name, student_grade)
-    @student_name = student_name
-    @student_grade = student_grade
-    @roster[student_grade] ||= []
-    @roster[student_grade] << student_name
+    roster[student_grade] ||= []
+    roster[student_grade] << student_name
 
   end
 
