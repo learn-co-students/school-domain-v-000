ad96da49a5577cd4eecfc02e756e7a85a1675b2f
jchu4483 <jchu4483@gmail.com>
2016-03-18 13:39:15 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
 # code here!
+
+class School
+  attr_reader :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+    grade[name] = []
+  end
+end
