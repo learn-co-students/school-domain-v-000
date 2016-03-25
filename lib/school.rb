ba5c656a686ed21a61b56567a2497c9944666bce
miclp <michaelpeimani@gmail.com>
2016-03-25 11:15:24 -0700

    add School class with basic features

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+
+class School
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_roster = {}
+    @roster.each do |grade, students|
+      sorted_roster[grade] = students.sort
+    end
+    sorted_roster
+  end
+
+end
