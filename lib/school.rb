7221de9a4a9af9f41937da43585758bdb224ca79
Matt Cassara <cassaram09@gmail.com>
2016-03-26 14:36:13 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+  def initialize(name)
+    @name = name
+    @hash = {}
+  end
+
+  def roster
+    @hash
+  end
+
+  def add_student(name, grade)
+    if !@hash.include?(grade)
+      @hash[grade] = []
+      @hash[grade] << name
+    else
+      @hash[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @hash[grade]
+  end
+
+  def sort
+    @hash.each do |grade, name|
+      name.sort!
+    end
+  end
+end
