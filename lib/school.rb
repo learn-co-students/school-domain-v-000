4d0b1f4b4026febdb685750f8b2246f28356aa7b
Matt Cassara <cassaram09@gmail.com>
2016-03-26 14:50:44 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -9,12 +9,8 @@ class School
   end
 
   def add_student(name, grade)
-    if !@roster.include?(grade)
-      @roster[grade] = []
-      @roster[grade] << name
-    else
-      @roster[grade] << name
-    end
+    @roster[grade] ||= []
+    @roster[grade] << name
   end
 
   def grade(grade)
