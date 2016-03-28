dcf2e0992ce1420a414047cd7a9cb634ae244b87
hangloos <ericloos00@gmail.com>
2016-03-28 17:17:16 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+
+class School
+
+def initialize(school)
+  @school = school
+  roster = {}
+  @roster = roster
+end
+
+def roster
+  @roster
+end
+
+def add_student(name, grade)
+if roster.include?(grade) == false
+  roster[grade] = []
+end
+  roster[grade] << name
+end
+
+def grade(number)
+  roster[number]
+end
+
+def sort
+sorted_roster = {}
+
+roster.each { |grade, name|
+sorted_roster[grade] = name.sort
+
+ }
+ sorted_roster
+
+
+end
+end 
