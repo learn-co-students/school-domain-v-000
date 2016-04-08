4a2c80345b726ad794024150c5eabbc32cfd3068
kade042 <dereje88@gmail.com>
2016-04-08 01:33:47 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+require 'pry'
+class School
+
+
+  def initialize(name)
+    @school = name
+    @roster = Hash.new
+  end
+
+  def roster
+    @roster
+   # binding.pry
+  end
+
+  def add_student(student, grade)
+    @roster[grade] = [] if roster.length == 0 || roster[grade] == nil
+    @roster[grade] << student
+    #binding.pry
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    new_h = @roster
+    @roster.each do |key,value|
+      new_h[key] = value.sort
+    end
+    new_h
+  end
+
+end
+#school = School.new("Black-lion")
+#school.add_student("dereje", 10)
+
+
