0d4d504688ed2efd27fa260afc0b2ae8ba8e3d72
Lush Sleutsky <lushsleutsky@gmail.com>
2016-04-16 15:22:58 -0400

    finished School class

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+    attr_accessor :school, :roster
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        roster[grade] ||= []
+        roster[grade] << student
+    end
+
+    def grade(grade_level)
+        roster[grade_level]
+    end
+
+    def sort
+        sorted = {}
+        roster.each do |grade, students|
+            sorted[grade] = students.sort
+        end
+
+        sorted
+    end
+end
