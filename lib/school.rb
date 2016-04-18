16b583dd2aa354817df6dd758572df5f20ac6330
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-04-18 17:45:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
 # code here!
+
+class School
+
+
+
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def roster
+   @roster
+  end
+
+  def add_student(name, grade)
+    if @roster.key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade]=[]
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each {|grade,names| names.sort!}
+
+    #@roster.sort_by do |grade, names|exit
+    
+     # names.collect do |names|
+      #  names
+      #end
+    #end
+
+  end
+
+
+end
