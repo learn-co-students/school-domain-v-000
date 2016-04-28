806bb4071f4d7c28d30cc9ea6b4d3b3e283d7b09
jd2rogers2 <jd2rogers2@gmail.com>
2016-04-28 15:11:34 -0400

    donzo

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster.include?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+
+  def grade(arg)
+    @roster[arg]
+  end
+
+  def sort
+    @sorted = {}
+    @roster.each do |grade, person|
+      @sorted[grade] = person.sort
+    end
+    @sorted
+  end
+
+end
