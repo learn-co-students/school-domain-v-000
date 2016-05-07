4582491e81d31441ee3a1c978b7d782e1764ce4d
Jonathan Stone <jon_p_stone@yahoo.com>
2016-05-07 02:36:04 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+
+  attr_reader :roster, :add_student
+
+  def initialize(roster)
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = []
+    if grade == @roster[grade]
+      @roster[grade] = []
+    else
+      @roster[grade] << name
+    end
+  end
+
+
+
+
+
+end
