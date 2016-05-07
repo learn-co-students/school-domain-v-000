1b8970e1cfff5452c9948fd2ec29943ba714ef40
Jonathan Stone <jon_p_stone@yahoo.com>
2016-05-07 03:35:20 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -12,4 +12,16 @@ class School
     roster[grade] << name
   end
 
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+  
 end
