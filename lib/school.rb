02682b6c703d455198fde5cde1d86e6922c3dd1d
rnhutsko <rnhutsko@gmail.com>
2016-05-12 16:26:15 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+
+class School
+  #attr_accessor :student
+  attr_reader :name
+
+  def initialize (name)
+    @name = name
+  end
+
+  def roster
+    roster ={}
+    @roster = roster
+  end
+
+  def add_student(student, grade)
+      roster[students]=[]
+      roster[students][grades_level] = []
+      roster[students] << student
+      roster[student][grades_level] << grade
+
+  end
+
+
+
+end # of class School
