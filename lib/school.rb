a220ef82d92f5b28597558c4a8923890e9d21836
Being Young <beingy@gmail.com>
2016-05-12 13:24:28 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+class School
+  attr_accessor :student, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] = [] unless @roster.include?(grade)
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.keys.map { |grade| @roster[grade].sort! }
+    @roster
+  end
+
+end
+
+# school = School.new("Bayside High School")
+# school.roster
+#
+# school.add_student("Zach Morris", 9)
+# school.roster
+#
+# school.add_student("AC Slater", 9)
+# school.add_student("Kelly Kapowski", 10)
+# school.add_student("Screech", 11)
+# school.roster
+# # => {9 => ["Zach Morris", "AC Slater"], 10 => ["Kelly Kapowski"], 11 => ["Screech"]}
+#
+# school.grade(9)
+# # => ["Zach Morris", "AC Slater"]
+#
+# school.sort
+# # => {9 => ["AC Slater", "Zach Morris"], 10 => ["Aardvark", "Kelly Kapowski"], 11 => ["Screech", "Xavier"]}
