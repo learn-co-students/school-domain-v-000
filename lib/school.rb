1a8b344a07531102c25307d1d58926a9cac92bc5
wedesignstudios <deelden@gmail.com>
2016-05-16 14:03:14 -0500

    added class School, coded methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if !@roster.has_key?(grade)
+    @roster[grade] << name
+  end
+
+  def grade(level)
+    @roster[level]
+  end
+
+  def sort
+    @roster.each {|grade, names| names.sort!}
+  end
+
+end
