93e930c919e08d37b5db70d377f66b93e3dd9212
Anita Ford <anitamford@gmail.com>
2016-05-15 21:44:05 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+  attr_accessor :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+
+    unless @roster.include?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @sorted_list = {}
+    @roster.each {|grade, students| @sorted_list[grade] = students.sort}
+    @sorted_list
+  end
+
+end
