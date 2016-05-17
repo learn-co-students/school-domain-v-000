b9e42e9194015b78e32dfd3a6ad16a040cac17ca
iandevivi <iandevivi@gmail.com>
2016-05-16 21:58:51 -0400

    completed school domain lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+class School
+  attr_accessor :grade
+  attr_reader :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def school
+    @school
+  end
+  
+  def add_student(name, grade)
+    if !roster.include?(grade) 
+      roster[grade] = []
+      roster[grade] << name
+    else
+      roster[grade] << name
+    end
+  end
+
+  def grade(grade_level)
+    roster[grade_level]
+  end
+
+  def sort
+    roster.each do |key, value|
+      value.sort!
+    end
+  end
+
+end
