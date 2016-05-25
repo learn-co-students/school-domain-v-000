31694234c0b8ac69675aebb134a4bf191ecd0270
tpstar <hanlee12@gmail.com>
2016-05-24 22:36:52 -0400

    code school domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  attr_reader :school, :roster
+
+  def initialize(school, roster ={})
+    @school = school
+    @roster = roster
+  end
+
+  def add_student(student, grade)
+    if !@roster[grade]   #check if @roster[grade] is nil
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
+
+end
