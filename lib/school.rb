9752386c626ceec0867da9c3e26a7bee0e16bd5b
Dom McKellar <dom.mckellar@gmail.com>
2016-05-25 07:44:59 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+    attr_accessor :roster
+
+    def initialize(school_name)
+      @school_name = school_name
+      @roster = {}
+    end
+
+    def add_student(student_name, grade_level)
+        roster[grade_level] ||= roster[grade_level] = []
+        roster[grade_level] << student_name
+    end
+
+    def grade(grade_level)
+      roster[grade_level]
+    end
+
+    def sort
+      sorted_hash = {}
+      roster.each do |grade_level,student_list|
+        sorted_hash[grade_level] = student_list.sort
+        # sorted_hash.store(grade_level, student_list.sort)
+      end
+      sorted_hash
+    end
+
+end
