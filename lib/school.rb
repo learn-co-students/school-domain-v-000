ff580cc5ab7424839a57f5abbbf85ce3d00f924b
Jennifer Pazos <jmp521@gmail.com>
2016-06-01 21:14:15 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,48 @@
+
 # code here!
+
+class School
+attr_accessor :name
+
+  def initialize(name)
+    @name = name
+
+  end
+  def roster=(roster)
+    @roster = roster
+
+  end
+  def roster
+    @roster = {}
+
+  end
+
+  def add_student=(name, grade)
+    @name = name
+    @grade = grade
+  end
+
+  def add_student(name, grade)
+    roster.each do |key, value|
+      if key == grade
+        if value.length == 0
+            roster[grade] = []
+
+        else
+          roster[grade]  << name
+        end
+      end
+    end
+  end
+
+
+
+  def grade(grade)
+    roster[grade]
+  end
+
+end
+
+school = School.new("Bayside High School")
+school.roster
+school.add_student("AC Slater", 10)
