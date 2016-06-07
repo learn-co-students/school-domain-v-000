318e93e61d04b6447247050c3c1df9783e8a532d
Lenn Hypolite <lenntriniman@hotmail.com>
2016-06-07 23:47:08 +0000

    initial commit passed first two test/ completed first two methods

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+
+#getter methods
+  attr_reader :student, :grade, :add_student  #returns information stored in each of these variables
+  #setter methods
+  attr_accessor :roster, :name
+
+#name initialize and settter method
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+#add_student setter method
+  def add_student(student, grade)
+    @student = student
+    @grade = grade
+    @roster[grade] = []
+    @roster[grade] << student
+  end
+
+
+end
