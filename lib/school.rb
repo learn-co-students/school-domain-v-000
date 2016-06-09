1e4615907bb080eabd80750259c6b9f5e71e18fa
Lenn Hypolite <lenntriniman@hotmail.com>
2016-06-09 00:23:08 +0000

    the sort method is incomplete

--- lib/school.rb
+++ lib/school.rb
@@ -1,34 +1,30 @@
-require "pry"
 class School
 
-#getter methods
-  attr_reader :student, :grade, :add_student  #returns information stored in each of these variables
-  #setter methods
-  attr_accessor :roster, :name
-
-#name initialize and settter method
+  #getter methods
+  attr_reader :student, :grade, :add_student, :roster, :name, :sort  #returns information stored in each of these variables
+  #don't need any setter values as you are not writting values
+  #name initialize and settter method
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-#add_student method
+  #add_student method
   def add_student(student, grade)
     @student = student
-    @grade = grade
-
-    #failed code
-    # if @roster[grade] = [] || @roster.include?(grade)
-    #   @roster[grade] << student
-    # else
-    #   @roster[grade] = student
-    # end
-
-    if roster[grade] == nil
-      roster[grade] = [student]
+    if roster[grade] == nil #check to see if there is nothing inside
+      roster[grade] = [student] #if it is then create a new grade key
     else
-      roster[grade] << student
+      roster[grade] << student #if it isn't and the key is already present then push the student value into it
     end
+  end
 
+  def grade(grade)
+    @grade = grade
+    @roster[grade] #retrieve students from a grade
+  end
+
+  def sort
+    @roster.sort.each { |grade, student|  student.sort } #sort the grades then sort the students
   end
 end
