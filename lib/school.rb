6e7333ad1469c9598a4cdb9105e43dfed1bc4ad5
Adam Taitano <adam.taitano@gmail.com>
2016-07-02 10:32:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+  end
+
+  def roster(roster = {})
+    @roster = roster
+  end
+
+  def add_student(student_name, grade)
+    roster[grade] = []
+    puts roster
+  end
+end
