7201ecdfd17151c1b998ef9b9f0cbc31ffc84f06
Anna Lansfjord <lansfjord@gmail.com>
2016-07-05 22:07:48 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
 # code here!
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster_hash = {}
+  end
+
+  def roster
+    @roster_hash
+  end
+
+  def add_student(student_name, student_grade)
+    if @roster_hash[student_grade] == nil
+      @roster_hash[student_grade] = []
+      @roster_hash[student_grade] << student_name
+    else
+      @roster_hash[student_grade] << student_name
+    end
+  end
+
+  def grade(student_grade)
+    @roster_hash[student_grade]
+  end
+
+  def sort
+    new_hash = Hash[@roster_hash.sort_by {|key, value| key}]
+
+    final_hash = {}
+    new_hash.each do |key, array|
+      array.sort!
+      puts array
+    end
+
+
+
+  end
+
+end
