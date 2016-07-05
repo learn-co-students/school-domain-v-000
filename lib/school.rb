03ab80e668af9d1eceea936fbfec33853076324f
auranbuckles <auran59@gmail.com>
2016-07-05 17:42:19 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,47 @@
-# code here!
+require 'pry'
+
+class School
+
+	def initialize(roster)
+		@roster = {}
+	end
+
+	def roster
+		@roster
+	end
+
+	def add_student(name, grade)
+		if @roster.key?(grade)
+			@roster[grade] << name
+		else
+			@roster[grade] = []
+			@roster[grade] << name
+		end
+	end
+
+	def grade(grade)
+		grade = @roster[grade]
+		return grade
+	end
+
+	def sort
+		new_roster = {}
+		@roster.each do |key, value|
+			# value.each do |name|
+			# 	# binding.pry
+			# 	value.sort
+			# end
+			new_roster[key] = value.sort
+		end
+		new_roster
+	end
+
+# {7 => ["Blake Johnson", "Jack Bauer"], 9 => ["Bart Simpson", "Homer Simpson"], 10 => ["Jeff Baird", "Avi Flombaum"]}
+
+
+end
+
+
+
+
+# school = School.new("Bayside High School")
