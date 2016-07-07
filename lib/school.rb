61c26c745122293221524b9a98cd84fe1815ba91
Onyekachi <onye@smmaven.com>
2016-07-07 01:57:26 -0700

    Done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+	def initialize(school)
+		@school = school
+		@hash = {}
+	end
+
+	def roster
+		@hash
+	end
+
+	def add_student(student, grade)
+		if @hash[grade] == nil
+			@hash[grade] = []
+			@hash[grade] << student
+		else
+			@hash[grade] << student
+		end
+	end
+
+	def grade(grades)
+		return @hash[grades]
+	end
+
+	def sort
+		@hash.each do |grades, student|
+			student.sort!
+		end
+		return @hash
+	end
+
+end
+
+	
