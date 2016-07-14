c4ed74c5baa64c5c606e51f974a8c3698e1e136a
Jason Casares <jasonc@csdrafting.com>
2016-07-14 15:32:21 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+
+  attr_accessor :roster, :name, :grade
+
+  def initialize(roster)
+    roster = {}
+    @roster = roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = []
+    @roster[grade] << "#{name}"
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.sort
+  end
+
+
+end
