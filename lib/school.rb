7c0bdafc2688f4f23cf799f0f719f6ffd58c97be
William Jones <enquiries@forestfed.com>
2016-08-01 06:15:09 +0000

    use hash and array methods to solve lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+  attr_accessor :roster, :grade, :student
+
+  def initialize(school)
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster.key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = [student]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+    @roster.sort
+    @roster
+  end
+
+end
