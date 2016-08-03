32ca79362e1684f359c312462bfab3f9b5161f46
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-08-03 01:13:50 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  def initialize(name)
+    @roster = {}
+  end
+  def roster
+    @roster
+  end
+
+
+  def add_student(name, grade)
+    roster[grade] ||=[]
+    roster[grade].push(name)
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each do |key, value|
+    value.sort!
+   end
+   return roster
+  end
+
+
+end
