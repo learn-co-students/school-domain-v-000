d33903d792045d80ead981211b8300fffb89a0ee
Justin Acker <justin@justinacker.com>
2016-08-21 17:46:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if !(@roster.include?(grade))
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(passed_grade)
+    @roster[passed_grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+    @roster
+  end
+
+end
