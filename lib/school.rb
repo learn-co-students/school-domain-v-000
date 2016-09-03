5c1985f8381c6dc20c2e2dcbe6f173ca9ae47e6f
vanita eckert <vanita112003@gmail.com>
2016-09-03 03:27:53 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+
+attr_reader :new, :roster
+
+roster = {}
+roster.length(0)
+
+  def initialize(new)
+    @new = new
+  end
+
+  def roster=(roster)
+    @roster = roster
+  end
+
+
+end
