7d2a90876994ca1ca27f2f1f47f8ecd321c97980
vanita eckert <vanita112003@gmail.com>
2016-09-05 22:15:39 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,29 @@
+require 'pry'
+
 class School
 
-attr_reader :new, :roster
+attr_accessor :student_name, :grade
+attr_reader :name, :roster
 
-roster = {}
-roster.length(0)
 
-  def initialize(new)
-    @new = new
+  def initialize(name)
+    @name = name
+    @roster = {}
   end
 
-  def roster=(roster)
-    @roster = roster
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
   end
 
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster = {}
+    @roster[grade] << student_name.sort
+  end
 
 end
