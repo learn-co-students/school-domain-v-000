9907836ec0a116674c647c6ecb7fcbdbaa2f10f0
vanita eckert <vanita112003@gmail.com>
2016-09-05 22:19:34 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -13,8 +13,8 @@ attr_reader :name, :roster
 
 
   def add_student(student_name, grade)
-    @roster[grade] ||= []
-    @roster[grade] << student_name
+    @roster[@grade] ||= []
+    @roster[@grade] << @student_name
   end
 
   def grade(grade)
