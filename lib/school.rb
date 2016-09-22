9f1791d1107a992c80e5a731c22ab0816278ce8b
Emmanuel Owusu <e_owusu@live.com>
2016-09-22 23:13:01 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry'
+
+class School
+  attr_accessor :grade, :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @grade = grade
+    @name = []
+    @name << name
+
+    @name.each do
+      if roster.key?(@grade) == true
+        roster[@grade] = @name
+      else
+        roster[@grade] = []
+        roster[@grade] = @name
+      end
+    end
+
+  end
+
+  def grade(grade)
+    roster[@grade]
+  end
+
+
+end
