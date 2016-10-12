80c80c3a46906b7eb847cfa3cac62ce500cf4ce0
Ray Mulligan <rmullig2@optonline.net>
2016-10-12 22:06:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+
+  def initialize(name)
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    !@roster.include?(grade) ? @roster[grade] = [] : nil
+    @roster[grade] << name
+  end
+
+  def grade(student_grade)
+    @roster[student_grade]
+  end
+
+  def sort
+    hashes = {}
+    @roster.each { |grade, students| hashes[grade] = students.sort }
+    hashes
+  end
+
+end
