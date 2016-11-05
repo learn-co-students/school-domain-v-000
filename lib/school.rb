c24aa0a2118da702fcf1b334faa4949c8d5c2c6d
Trever Andersen <trever.r.andersen@gmail.com>
2016-11-05 01:31:53 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    @roster.map do |grade, students|
+
+      @roster[grade]= students.sort
+    end
+    @roster
+  end
+
+end
