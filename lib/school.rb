4355384873435ee800c8ac67b9aff116bcd7dec3
Alexis Roman <alexismmroman21@gmail.com>
2016-11-08 14:54:02 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+
+  def initialize(school)
+    @school = school
+
+  end
+
+  def roster
+    @roster = {}
+
+  end
+
+  def add_student(name,grade)
+
+    @roster[grade] ||= []
+
+    @roster[grade] << name
+
+  end
+
+
+end
