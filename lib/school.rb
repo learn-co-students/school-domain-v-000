965473bb21bbb3402eb6d683e210b6b45f14dd25
Scott Wakeman <swakeman@ca.rr.com>
2016-11-21 20:10:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new {|h, k| h[k] = []} # has empty roster when initialized
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    # if self.roster[grade] already exists leave it alone!
+    # else set self.roster[grade] = []
+    #self.roster[grade] ||= []
+    self.roster[grade] << name # is able to add multiple students to a class (grade)
+    #else
+      #@roster[grade] = [name] # is able to add a student
+    #end
+  end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+  def sort
+    self.roster.each do |grade, students|
+      students.sort!
+    end
+
+  end
+end
