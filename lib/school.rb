df14b4608f05be9e64555c429900da3dbd77ed1d
Elvin <elvin@elvingarcia.com>
2016-11-20 22:29:21 -0500

    Done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+
+class School
+  attr_reader :roster
+
+    def initialize(obj)
+      @roster = Hash.new {}
+    end
+
+    def add_student (value, key)
+      if @roster.key?(key)
+        @roster[key]. push(value)
+      else
+        @roster[key] = [value]
+      end
+    end
+
+    def grade(key)
+      @roster[key]
+    end
+
+    def sort
+      @roster[9].sort!
+      @roster.sort_by{|k,v|k}.to_h
+    end
+
+end
