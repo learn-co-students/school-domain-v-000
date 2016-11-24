03b4401787b7ebaa3bd496e483daf4955922594d
Senad Hadrovic <senadhadrovic@gmail.com>
2016-11-24 04:06:11 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,5 @@
 # code here!
+require 'pry'
 
 class School
   attr_accessor :roster
@@ -9,16 +10,21 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster[grade].empty?
-        @roster[grade] = []
+    if @roster.has_key? grade
           @roster[grade] << name
     else
-      @roster[grade] << name
-    end 
+      @roster[grade] = []
+        @roster[grade] << name
+    end
   end
 
   def grade(grade)
-    return roster[grade]
+    @roster[grade]
   end
 
+ def sort
+   @roster.each do |key, value|
+     @roster[key] = value.sort
+   end
+ end
 end
