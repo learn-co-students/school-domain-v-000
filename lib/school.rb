91d9dc752c0c9cf66b0475647b64147959b34eea
Senad Hadrovic <senadhadrovic@gmail.com>
2016-11-24 03:30:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade].empty?
+        @roster[grade] = []
+          @roster[grade] << name
+    else
+      @roster[grade] << name
+    end 
+  end
+
+  def grade(grade)
+    return roster[grade]
+  end
+
+end
