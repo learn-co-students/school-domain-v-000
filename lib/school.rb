eafc13acb42f0cc2d26d29668e7434dd4f4b1e51
Adam Lucia <lucia.adam@gmail.com>
2016-12-09 20:46:11 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+
+  attr_reader :name, :roster
+
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student (student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+  end
+
+  def grade (grade)
+    @roster[grade]
+  end
+
+  def sort
+    @sorted = {}
+    @roster.each_pair { |grade, student_array| @sorted[grade] = student_array.sort }
+    Hash[@sorted.sort]
+  end
+end
