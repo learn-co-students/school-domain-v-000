6bc0e71b491c9cd8a6edeb9a4d7ab36b7a43eece
Jonathan Foster <redcabinny@gmail.com>
2017-01-07 20:25:24 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+def add_student(student_name, grade)
+  if !@roster[grade]
+    @roster[grade] = []
+  end
+  @roster[grade] << student_name
+end
+
+def grade(grade)
+  (@roster.select do |grade_level, student_array|
+    grade == grade_level
+  end).values[0]
+end
+
+def sort
+  sorted_hash = {}
+  @roster.sort.each do |key, array|
+    sorted_hash[key] = array.sort
+  end
+  sorted_hash
+end
+
+end
