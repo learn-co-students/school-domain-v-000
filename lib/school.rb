dcf1b153f415a23452ca4a2f3f3cdfb618950581
Terry Thompson <terry.lyn.thompson@gmail.com>
2017-01-08 22:49:13 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+require 'pry'
+class  School
+attr_writer :school_name, :student_name
+
+def roster
+roster = Hash.new
+end
+
+def initialize(school_name)
+  @name = school_name
+end
+
+def add_student(student_name, grade)
+  binding.pry 
+  roster[grade] = []
+roster[grade] << student_name
+end
+
+def grade(num)
+  roster[num].values
+end
+
+end
