590dc78fc0457b5e67941843f5547b9e6e96bec2
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-01-09 09:49:37 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,39 +1,24 @@
 # code here!
-<<<<<<< HEAD
 class School 
-attr_accessor :roster :student_name :school_name
+attr_accessor :roster, :student_name, :school
 
-def initialize(name)
-  @name = name
+def initialize(school)
+  @school = school
+  @roster = Hash.new
 end
 
 
 def add_student(student_name, grade)
-  roster[grade] = []
-  roster[grade] << student_name
+  if @roster == {} 
+    @roster[grade] = []
+    @roster[grade] << student_name
+  else 
+    @roster[grade] << student_name
   end
-=======
-require 'pry'
-class  School
-attr_writer :school_name, :student_name
-
-def roster
-roster = Hash.new
-end
-
-def initialize(school_name)
-  @name = school_name
-end
-
-def add_student(student_name, grade)
-  binding.pry 
-  roster[grade] = []
-roster[grade] << student_name
 end
 
 def grade(num)
-  roster[num].values
+  @roster[num].values
 end
->>>>>>> dcf1b153f415a23452ca4a2f3f3cdfb618950581
 
 end
