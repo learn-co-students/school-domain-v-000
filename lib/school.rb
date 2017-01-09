7e33e4d0331f36a7a6764b6499c487ec170a52e3
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-01-09 09:32:52 -0600

    home update

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
 # code here!
+class School 
+attr_accessor :roster :student_name :school_name
+
+def initialize(name)
+  @name = name
+end
+
+
+def add_student(student_name, grade)
+  roster[grade] = []
+  roster[grade] << student_name
+  end
+
+end
