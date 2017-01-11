6cbcbb5d1ad2adc0505d1f4e8f3614645b4f6a56
Alex Glover <alexanderbglover@gmail.com>
2017-01-11 15:59:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+require "pry"
+
+class School
+  attr_reader :school_name
+  ROSTER = {}
+
+  def initialize(school_name)
+    @school_name = school_name
+  end
+
+  def roster
+    ROSTER
+  end
+
+  def add_student(name,grade)
+      ROSTER[grade]||=[]
+      ROSTER[grade]<<name
+  end
+
+end
