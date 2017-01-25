64c9037cc21c2d5a0a8a32cb09392e5f62ef5264
Parv Kumar <pkumar@trinity.edu>
2017-01-25 21:33:05 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+
+  attr_writer :add_student
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] =[] unless @roster.include?(grade)
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    return @roster.values_at(grade).flatten
+  end
+
+  def sort
+    @roster.each do |keys,values|
+      values.sort!
+    end
+    
+  end
+
+end
