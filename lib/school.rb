168e60a273abb7ef17862b0a4ac9987c446d9ebc
charlie lee <ctlee9096@gmail.com>
2017-01-29 23:57:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -5,19 +5,19 @@ class School
     @roster = {}
     @school = school
   end
-  def add_student(value,key)
-    binding.pry
-    roster[key] = []
-    if roster[key] = nil && roster[value] = nil
-    binding.pry
-    roster[key] << value
-    binding.pry
-  elsif roster[key] = key && value != nil
+  def add_student(grade,name)
     #binding.pry
-    roster[key] = []
-    roster[key] << value
-    #roster[new_key].delete(new_value_for_value_array)
-    binding.pry
+    roster[name] = []
+    if roster[name] = nil && roster[grade] = nil
+    #binding.pry
+    roster[name] << grade
+    #binding.pry
+  elsif roster[name] = name && grade != nil
+    #binding.pry
+    roster[name] = []
+    roster[name] << grade
+    #roster[new_name].delete(new_grade_for_grade_array)
+    #binding.pry
     end
   end
 end
