6547a1a601f4ce22a7648175a19c240421022323
Damien de Lartigue <damien@delartigue.co.uk>
2017-01-31 00:24:16 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+
+class School
+  #attr_accessor
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade].nil?
+      @roster[grade] = [name]
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, name_array|
+      name_array.sort!
+    end
+  end
+  @roster
+end
