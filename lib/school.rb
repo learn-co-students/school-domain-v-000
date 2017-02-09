bd92df2b0d6cbcfd547d2405f1a245c5f861b4af
Alex Crisan <alexcrisan2005@hotmail.com>
2017-02-09 17:10:26 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
 # code here!
+
+class School
+  attr_accessor :roster, :student_name, :school_name
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade]
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+end
+
+def sort
+  @roster.collect do |grade|
+    grade.sort!
+  end
+end
+
+
+#
+# bayside = School.new("Bayside High School")
+#
+#
+# puts bayside.school_name
+#
+# bayside.add_student("billy", 10)
+#
+# bayside.add_student("bobby", 10)
+#
+# bayside.add_student("joey", 12)
+# puts bayside.roster
