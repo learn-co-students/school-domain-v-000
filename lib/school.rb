b526a8e611b47001e87ab2858d3cd37b4c05dec4
Eric Levy <levyeric10@gmail.com>
2017-03-06 02:42:24 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] ||= []
+    @roster[grade] << name
+  end
+  end
+
+  def grade(grades)
+    @roster[grades]
+  end
+
+  def sort
+    @roster.values
+  end
+
+
+
+end
