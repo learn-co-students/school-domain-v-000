ea71c3a73866057a424974ea80e59543ffb1db86
Rachel Walwood <walwoodr@gmail.com>
2017-03-10 22:48:21 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] unless @roster[grade]
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each { |grade, grade_student_array| grade_student_array.sort! }
+  end
+end
