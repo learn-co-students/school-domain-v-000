29ea7bb11b7294f6ecc03ca0f9f421a5ad897508
Spencer Evans <spencevans@gmail.com>
2017-03-10 20:48:52 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -9,18 +9,23 @@ class School
   end
 
   def add_student(name, grade)
-    if @roster[grade]
-      @roster[grade] << name
-    else @roster[grade] = [name]
+    if roster[grade]
+      roster[grade] << name
+    else roster[grade] = [name]
   end
 end
 
   def grade(grade)
-    return @roster[grade]
+    return roster[grade]
   end
 
   def sort
-    @roster.sort_by {|student_grade, names_array| names_array }
+    #new_roster = {}
+    #@roster.each {|student_grade, names_array| new_roster[student_grade] = names_array.sort }
+    #new_roster
+
+    roster.each {|grade, students| students.sort!}
+    roster
   end
 
 end
