f8743d6e20ba8ca5c0dc335286b824a5a4c1c2e5
thesilentc <thesilentc@me.com>
2017-03-18 15:51:23 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+  @roster = {}
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+end
+# def initialize(school)
+#   puts roster{}
+#
+# end
