bd71ec9b64665cf5e827cc997daf352747d08072
thesilentc <thesilentc@me.com>
2017-03-18 16:01:19 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,10 @@ class School
     @roster = {}
   end
 
+  def add_student(student_name, grade)
+    roster[grade] = []
+    roster[grade] << student_name
+  end
+
+
 end
-# def initialize(school)
-#   puts roster{}
-#
-# end
