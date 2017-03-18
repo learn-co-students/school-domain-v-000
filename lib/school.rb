2094d2f7a3049ea52c164dabba7c09d7e995dcd8
thesilentc <thesilentc@me.com>
2017-03-18 16:08:31 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -16,5 +16,13 @@ class School
     roster[student_grade]
   end
 
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+
 
 end
