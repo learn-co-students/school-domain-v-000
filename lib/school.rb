b2e493dd0cb15b19ef86a4904b81f39637e34641
William Barela <william.barela.dev@gmail.com>
2017-03-27 08:51:17 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_accessor :school
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name #if @roster.all?{|grade, subroster| subroster.none?{|student| student == name}}
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each{|grade, subroster| @roster[grade] = subroster.sort}
+  end
+  
+end
