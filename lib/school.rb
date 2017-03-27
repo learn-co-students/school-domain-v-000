cd732fb96ae3d2e4bd18c104b9f2e7c5c1411805
William Barela <william.barela.dev@gmail.com>
2017-03-27 09:15:26 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -13,6 +13,7 @@ class School
   def add_student(name, grade)
     @roster[grade] ||= []
     @roster[grade] << name #if @roster.all?{|grade, subroster| subroster.none?{|student| student == name}}
+    #sort #This would be a good place to run sort to keep the list sorted
   end
 
   def grade(grade)
@@ -20,7 +21,7 @@ class School
   end
 
   def sort
-    @roster.each{|grade, subroster| @roster[grade] = subroster.sort}
+    @roster.each{|grade, subroster| subroster.sort!}
   end
-  
+
 end
