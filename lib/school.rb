830e9ce9b5ba5cab4b286a8f7bda06788a158e41
Liliana Alonzo <lillyneveu@gmail.com>
2017-03-28 01:15:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+  end
+
+  def grade(student_grade)
+    @roster[student_grade]
+  end
+
+  def sort
+    sorted_roster = {}
+    @roster.each { |grade, student_name| sorted_roster[grade] = student_name.sort }
+    sorted_roster
+  end
+  # Does not alter the original hash permanently
+
+end
