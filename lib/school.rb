ed651b26be678793c9f478b6cc481a30a244dddf
Michael Ries <michael.ries@gmail.com>
2017-04-04 15:28:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,27 @@
 class School
-  attr_assessor :roster, :name:
+  attr_accessor :roster, :name
   
   def initialize(name)
     @name = name
     @roster= {}
+  end
+
+  def add_student(names,grade)
+     if @roster[grade]
+       @roster[grade] << names
+     else
+       @roster[grade] = [names]
+     end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_array = @roster.keys.sort
+    new_roster = {}
+    sorted_array.each { |index| new_roster[index] = @roster[index].sort }
+    @roster=new_roster
   end
 end
