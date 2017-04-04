677b21c813ddd1e7840a4a84aa1a30225fa7707f
Eva Navarror <emanao@me.com>
2017-04-04 19:32:31 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+# code here
+class School
+  def initialize(name)
+    @name=name
+    @roster={}
+
+  end
+  def roster
+    @roster
+  end
+  def add_student(student, grade)
+    @roster[grade] = [] if !@roster.include?(grade)
+    @roster[grade]<<student
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    @roster.collect do|grade, student_array|
+       student_array.sort!
+    end
+    @roster
+  end
+end
