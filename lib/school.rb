93d4bc76aea260671ac0810eaa1b53e46f0573e4
github username <github email address>
2017-04-05 19:06:36 +0900

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -4,24 +4,21 @@ class School
   attr_reader :roster
   def initialize(name)
     @name = name
-    @roster = {}
+    @roster = {} #이건 왜 def로 새로 만들면 적용이 안되는거지? .name / .roster할때 뭐때문에 접근이 가능한거지
   end
 
   def add_student(student_name,grade)
     @student_name = student_name
     @grade = grade
 
-    if !roster.keys.include?(@grade)
-      @roster[@grade] = []
-      @roster[@grade] << @student_name
-    else
-      @roster[@grade] << @student_name
-    end
+    roster[grade] = [] if !@roster.keys.include?(grade)
+    roster[grade] << student_name
   end
 
   def grade(grade)
     @roster[grade]
   end
+  
   def sort
     @roster= Hash[@roster.sort_by { |grade, name| grade}]
     @roster.collect {|grade,name| name.sort!}
