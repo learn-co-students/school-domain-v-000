7f0c2278902cc7d21ad8123770a12c88398443b7
David Mars <david.w.mars@my.occc.edu>
2017-04-12 22:49:17 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+#   ~~~HAVICK WAS HERE~~~
+
+class School
+  attr_accessor  :roster, :name
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+
+
+def length
+  @length = roster.length
+end
+
+def add_student(name, grade)
+  @roster[grade] = [] if !(@roster.include?(grade))
+  @roster[grade] << "#{name}"
+
+end
+
+def grade(grade)
+  @roster[grade]
+end
+def sort
+  sorted = Hash[@roster.sort_by {|grade, name| grade}]
+sorted
+end
+end
