ce1407ccc90f00f328d56a9c83dcac526184b98b
Harleigh Abel <harleighabel@gmail.com>
2017-04-15 10:39:47 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader :name, :grade
+
+  def initialize(title)
+    @title = title
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster.include? (grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |k, v|
+        v.sort!
+    end
+  end
+    
+end
