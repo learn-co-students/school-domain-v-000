d655b3a1cc59f2a16dc1b3f4a87c54752a39bfcd
Saad <Dropheaven@live.com>
2017-04-14 20:18:19 -0600

    Lab complete

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  attr_reader :name, :roster, :add_student, :grade, :sort
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+  def add_student(student, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << student
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(students_grade)
+    @roster[students_grade]
+  end
+
+  def sort
+    @roster.each do |grade, student|
+      @roster[grade] = student.sort
+    end
+    @roster
+  end
+
+end
