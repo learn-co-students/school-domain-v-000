69db6b53149f7584ba0697421acf058fb0243c8e
Michael Mirzakandov <mikemirzak@aol.com>
2017-04-20 14:31:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_reader :school_name, :add_student, :grade
+  attr_accessor :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end 
+  
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+    if roster.include?(grade) != true
+      roster[grade] = []
+    end
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    @roster.sort.each_with_object({}) do |(grade_key, name_arrays), name |
+      name[grade_key] = name_arrays.sort
+      name.sort
+    end
+  end
+    
+end
