1d31afbd8cb9c53a8626cc2d50bdd40f66ab4b3f
lucketta <lucketta@umich.edu>
2017-04-20 01:38:02 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if !@roster[grade]
+      @roster[grade] = []
+      @roster[grade] << student
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade_level, student_array|
+      student_array.sort!
+    end
+  end
+
+
+end
