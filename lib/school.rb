63659eb7d00c0995b4f92598deeb7a3cfc50bb2d
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-04-30 17:42:09 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+   attr_accessor :name , :grade
+    attr_reader :roster
+  
+     def initialize(roster)   
+         @roster = roster
+         roster.clear
+     end
+     
+  def add_student(name,grade)
+      
+       @roster[grade] << name
+     
+   end
+   
+   
+ 
+ 
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+end
