87df056d5b18c240246a938b907d4c6ca000efcb
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-05-02 01:03:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require 'pry'
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, integer)
+    if @roster[integer]
+      @roster[integer] << name
+    else
+      student_name = []
+      student_name << name
+      @roster[integer] = student_name
+    end
+  end
+
+  def grade(int)
+    @roster[int]
+  end
+end
