cb7c30faf8ce263e186b7a9ec901b950098375b3
Stephen Harrington <stephenharrington04@yahoo.com>
2017-05-03 13:52:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,30 +1,24 @@
-# code here!
 class School
 
-  def initialize(school_name)
-    @school_name = school_name
+  attr_accessor :roster
+
+  def initialize(roster)
     @roster = {}
   end
 
-  attr_accessor :name, :roster
-
-  def add_student(student_name, grade_level)
-    if roster.keys.include?(grade_level)
-      roster[grade_level] << student_name
-    else
-      roster[grade_level] = []
-      roster[grade_level] << student_name
-    end
+  def add_student(student_name, grade)
+    roster[grade] = [] unless roster.include?(grade)
+    roster[grade] << student_name
   end
 
-  def grade(grade_level)
-    roster[grade_level]
+  def grade(grade)
+    roster[grade]
   end
 
   def sort
     sorted_students = {}
     roster.each do |grade, students|
-      sorted_students[grade] = roster[grade].sort
+      sorted_students[grade] = students.sort
     end
     sorted_students
   end
