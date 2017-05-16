46371ea69c19970b3c9543896367cc4116098c63
Alex <github email address>
2017-05-15 22:11:36 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -26,6 +26,8 @@ class School
     @roster[grade]
   end
 
+#setup sort, perform each on each value in the hash which needs to arguments
+# at each key for the hash, take the list of values and sort them
   def sort
     @roster.each do |key, value|
       @roster[key] = value.sort
