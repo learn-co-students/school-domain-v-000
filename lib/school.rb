32151c459a2dfbfc2718ac33cb003eb23c1de616
Alex <github email address>
2017-05-15 22:10:28 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+
+class School
+
+  attr_accessor :roster
+
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]==nil
+        @roster [grade]= []
+        @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+
+  end
+
+#call the values for the given grade in the roster hash
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      @roster[key] = value.sort
+    end
+  end
+end
