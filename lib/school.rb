e2baf41dfb2a33472b0875c0eec7968b54676e98
Menachem Hornbacher <me@mendel.tech>
2017-05-19 14:14:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] = [] if !@roster.include? grade
+    @roster[grade] << student_name
+    @roster
+  end
+
+  def grade(grade)
+    @roster.include?(grade) ? @roster[grade] : {}
+  end
+
+  def sort
+    res = {}
+    @roster.sort.to_h.each { |key, value| res[key] = value.sort }
+    res
+  end
+end
