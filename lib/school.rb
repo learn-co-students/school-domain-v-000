6e94cf0dae371d2d152d7dfd817eaa77a5b58ed8
Sullivan Nolan <nolakat@gmail.com>
2017-05-20 10:19:15 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -11,12 +11,12 @@ class School
   
   def add_student(student, grade)
     #If the grade doesn't exsist then create an empty key/pair and push grade key value into it 
-    if @roster[grade]== nil 
-        @roster[grade] = []
-        @roster[grade] << student
+    if roster[grade]== nil 
+        roster[grade] = []
+        roster[grade] << student
     else
       #add students to exsisting grade 
-    @roster[grade] << student
+    roster[grade] << student
   end
 end
 
