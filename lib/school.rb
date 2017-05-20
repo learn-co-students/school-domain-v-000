ba3e6e19cbdb49d7f85eb702e675425ed17caaa8
Kristy Peterson <kpeterson.kap@gmail.com>
2017-05-20 08:04:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+attr_accessor :school_name, :roster
+
+  def initialize(name) #passing in the name of the SCHOOL and setting it = to Instance Var
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster.empty? || @roster[grade] == nil #does the hash roster containe any values?  Or does the key exist in the hash?
+      @roster[grade] = []  #create an array for the grade key
+      @roster[grade] << student #push the student name into the array for that key
+    else
+      @roster[grade] << student  #the key exists so add additional name to array
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each_value { |students| students.sort!}
+  end
+end
