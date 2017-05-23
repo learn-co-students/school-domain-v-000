93ead39fedffa9311277995759503eae3050161d
Sam Marshall <samuel.r.marshall@gmail.com>
2017-05-23 03:31:56 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+GRADES = []
+NAMES = []
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+
+  attr_accessor :name, :roster
+
+  def add_student(name, grade)
+    if roster[grade] == nil
+      roster[grade]=Array.new<<name
+    else
+      roster[grade]<<name
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+  def sort
+    roster.each do |key,value|
+      value = value.sort!
+    end
+    roster
+  end
+end
