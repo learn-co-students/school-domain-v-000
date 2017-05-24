b3e5ae1b30540ed58f207312801960779f680b2f
gb23 <gregbenj@hotmail.com>
2017-05-23 17:05:54 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+class School
+    def initialize(school_name)
+        @school = school_name
+        @roster = {}
+    end
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        #given grade, look to see if has a key of grade
+            #if yes,
+                #shovel into array
+            #if no,
+                #point grade to newly created empty array, shovel in name
+        if @roster.has_key?(grade)
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(num)
+        final_array = []
+        @roster.each{ |grade, names|
+            if grade == num
+                names.each{ |name|
+                    final_array << name
+                }
+            end
+        }
+        final_array
+    end
+
+    def sort
+        @roster.each{ |grade, names|
+            names.sort!
+        }
+    end
+end
