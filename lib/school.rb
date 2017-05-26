261cf5af04efe5e403607e59c69db705c7598c0b
Elysa Silveira <elysa.silveira@gmail.com>
2017-05-26 21:05:27 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+  attr_accessor :roster 
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end 
+
+  def add_student(name, grade)
+    unless roster.include?(grade)
+      roster[grade] = []
+    end 
+    roster[grade] << name
+  end 
+
+  def grade(grade)
+    return roster[grade]
+  end
+
+  def sort
+    rost_sorted = Hash[roster.sort_by {|key, val| key}]
+    rost_sorted.each do |key, value|
+      if value[0] > value[1]
+        value[0], value[1] = value[1], value[0]
+      end 
+    end 
+  end 
+  
+end 
