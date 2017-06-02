0a594333d573de187d01c5b3803c1f880a28b7e2
John Blank <veloblank@gmail.com>
2017-06-02 10:51:15 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+          #'pipe equals'? Need to figure out grade issue, if grade exists...
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+          #related to add_student 'cleaning up'
+  def grade(grade)
+    @roster[grade]
+  end
+
+        #related to grade thing
+  def sort
+    sorted_list = {}
+    @roster.each do |grade, student|
+      sorted_list[grade] = student.sort
+    end
+    return sorted_list
+  end
+end
