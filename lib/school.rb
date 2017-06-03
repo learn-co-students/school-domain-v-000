a716d8d9aa98fb974785a9efb27f3b987de9fd14
Ninian Bond <gentile.bond@gmail.com>
2017-06-03 22:45:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+class School
+  ARRAY = []
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+def roster
+  @roster
+end
+
+def add_student(student, grade)
+  if @roster.keys.include?(grade)
+    @roster[grade] << student
+  else
+    @roster[grade] = []
+    @roster[grade] << student
+  end
+end
+
+  def grade(grade)
+    @grade = @roster[grade]
+  end
+
+  def sort
+    @sort = @roster.map do |grade|
+      @roster[grade].sort!
+    end
+  end
+
+
+
+end
