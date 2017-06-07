3a0c3a03dd4de7ac9e9a3aa3ef2efac4c5777e27
Nicholas D'Amico <nickalan82@icloud.com>
2017-06-07 01:09:26 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
+require "pry"
+
 # code here!
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    unless @roster[grade].is_a?(Array)
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+  end
+
+  def grade(grade_level)
+    @roster[grade_level]
+  end
+
+  def sort
+    sorted_roster = {}
+   @roster.each do |grade,student|
+      sorted_roster[grade] = student.sort
+    end
+    sorted_roster
+  end
+
+end
