97ff30eb4891a9c2cf4b1a72b94372aeded7b101
Sydney Chun <schun08@gmail.com>
2017-06-20 01:57:09 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+  attr_reader :roster
+
+  def initialize(name)
+    @school = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    #check if the grade exists
+    #if it does not, add it and then add the student
+    if !@roster.keys.include?(grade)
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+    #iterate over the hash and sort each array
+    @roster.each {|k,v| @roster[k] = v.sort!}
+  end
+
+end
