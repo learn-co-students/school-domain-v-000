4f0843cd66b8a454311506635413e330d194e567
lpassamano <leighayna.josele@gmail.com>
2017-06-28 20:12:31 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, student_array| #take key/hash pair as argument
+      #change the order of each array associated with the keys
+      @roster[key] = student_array.sort
+    end
+  end
+
+end
