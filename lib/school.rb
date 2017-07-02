9512bb18dc1f98485a4492d33e877744216d4701
jonathan <jonathanyi81@gmail.com>
2017-07-01 21:29:12 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+  #ROSTER = {}
+
+  def initialize(school)
+    @school=school
+    @roster={}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+    if @roster.key?(grade)
+      @roster[grade]<<name
+    else
+      @roster[grade]=[]
+      @roster[grade]<<name
+    end
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+    Hash[@roster.sort_by{|k,v| v.sort!}]
+  end
+end
