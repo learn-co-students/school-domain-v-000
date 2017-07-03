413464071ca994cd524a9d9a119677b08d147f88
Sydney Chun <schun08@gmail.com>
2017-07-03 18:54:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,28 +1,53 @@
-class School
+# first attempt 6/18/17
+# class School
+#
+#   attr_reader :roster
+#
+#   def initialize(name)
+#     @school = name
+#     @roster = {}
+#   end
+#
+#   def add_student(student, grade)
+#     #check if the grade exists
+#     #if it does not, add it and then add the student
+#     if !@roster.keys.include?(grade)
+#       @roster[grade] = []
+#     end
+#     @roster[grade] << student
+#   end
+#
+#   def grade(num)
+#     @roster[num]
+#   end
+#
+#   def sort
+#     #iterate over the hash and sort each array
+#     @roster.each {|k,v| @roster[k] = v.sort!}
+#   end
+#
+# end
 
-  attr_reader :roster
+# second attempt 7/3/17
+class School
+  attr_accessor :roster
+  attr_reader :name
 
   def initialize(name)
-    @school = name
+    @name = name
     @roster = {}
   end
 
-  def add_student(student, grade)
-    #check if the grade exists
-    #if it does not, add it and then add the student
-    if !@roster.keys.include?(grade)
-      @roster[grade] = []
-    end
-    @roster[grade] << student
+  def add_student(student_name, grade)
+    roster[grade] = [] unless roster.keys.include?(grade)
+    roster[grade] << student_name
   end
 
-  def grade(num)
-    @roster[num]
+  def grade(grade)
+    roster[grade]
   end
 
   def sort
-    #iterate over the hash and sort each array
-    @roster.each {|k,v| @roster[k] = v.sort!}
+    roster.each {|grade, students| roster[grade] = students.sort!}
   end
-
 end
