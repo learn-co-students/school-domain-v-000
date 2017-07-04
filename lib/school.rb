f294b01bf9efa75dd6cf223fd94445820fa135a8
Sam Crowder <samecrowder@gmail.com>
2017-07-03 20:32:54 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+
+class School
+
+	attr_accessor :name, :roster
+
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def add_student(name, grade)
+		if roster[grade] != nil
+			roster[grade].push(name)
+		else
+			roster[grade] = [name]
+		end
+	end
+
+	def grade(grade)
+		roster[grade]
+	end
+
+	def sort
+		roster.each do |grade, students|
+			students.sort!
+		end
+
+
+		#roster.sort.to_h
+	end
+end
