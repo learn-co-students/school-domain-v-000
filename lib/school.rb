cf184b2ec15dd7482c9f675c848910e0773ead92
Ary Baldioceda <abaldioceda@gmail.com>
2017-07-04 22:09:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,26 +1,26 @@
 # code here!
 class School
-  attr_accessor :roster
+  attr_accessor :roster, :school_name
 
   def initialize(school_name)
-    @school = school_name
+    @school_name = school_name
     @roster = {}
   end
 
   def add_student(name, grade)
-    @roster[grade] ||= []
-    @roster[grade] << name
+    roster[grade] ||= []
+    roster[grade] << name
   end
 
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
 
   def sort
-    @roster.collect do |key, array|
-      array.sort!
+    roster.collect do |key, array_of_students|
+      array_of_students.sort!
     end
-    @roster
+    roster
   end
 
 end
