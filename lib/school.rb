03c7b1cc68b19d5e71c8b39ff3b83d6c36d5274f
hansenjl <jenn.leigh.hansen@gmail.com>
2017-07-06 10:48:50 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def name
+    @name
+  end
+  def roster
+    @roster
+  end
+  def add_student(student,grade)
+    @roster[grade] = [] if @roster.keys.include?(grade) != true
+    @roster[grade] << student
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    @roster.each do |grade,students|
+      students.sort!
+    end
+  end
+end
+
+
+
+
