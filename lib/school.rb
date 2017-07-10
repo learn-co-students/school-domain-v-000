8edea9d5d08f30aed461084ad71ea83f7403f45f
Katie Larson <katmae83@gmail.com>
2017-07-10 17:55:26 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster.any? {|grade_key, name_array| grade_key == grade}
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @sorted_roster = {}
+    @roster.each do |grade, name|
+      sorted_name = name.sort {|kid_1, kid_2| kid_1 <=> kid_2}
+      @sorted_roster[grade] = sorted_name
+    end
+    @sorted_roster
+  end
+end
