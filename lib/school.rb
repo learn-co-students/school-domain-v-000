6babad7ca2e7be12ff8d2372a135adae29d33502
Dalia Sawaya <sawaya.dalia@gmail.com>
2017-07-11 00:37:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    @roster.collect do |grade, name|
+      sorted[grade] = name.sort
+    end
+    sorted
+  end
+
+end
