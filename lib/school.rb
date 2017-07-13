c399837a85bca3f3d513e06a82ac34357b4788b1
Dennis Nastasi <dcnastasi@gmail.com>
2017-07-12 22:16:26 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+class School
+
+  attr_reader :name, :roster, :grade
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @name = name
+    @grade = grade
+    unless @roster.has_key?(@grade)
+      @roster[@grade] = []
+      @roster[@grade] << @name
+    else
+      @roster[@grade] << @name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort()
+    @roster.map do |grade, name|
+      @roster[grade] = @roster[grade].sort
+    end
+    @roster
+  end
+
+end
+
+school = School.new("Flatiron School")
+binding.pry
