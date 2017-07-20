7ae3d28c80a1f076eb523848e1b481c428c05b74
Dominique De Leon <dominique.a.deleon@gmail.com>
2017-07-20 16:26:13 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+
+# require 'pry'
+
+class School
+
+  attr_accessor :roster
+
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+
+  end
+
+# binding.pry
+
+
+  def add_student(student,grade)
+    if roster.has_key?(grade)
+      roster[grade] << student
+    else
+      roster[grade] = []
+      roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+      roster[grade]
+  end
+
+  def sort
+    roster.map do|grade,student|
+      roster[grade] = student.sort
+    end
+    roster
+  end
+end
