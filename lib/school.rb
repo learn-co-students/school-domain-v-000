355b7db47f3af16ce2e098153c70ffea413ce76e
Donna <github email address>
2017-07-31 23:15:47 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+	def initialize(roster)
+		@roster = {}
+	end
+
+	attr_accessor :name
+	attr_reader :roster
+
+
+
+	def add_student(student_name, grade)
+		@roster[grade] ||= grade
+		@roster[grade] << student_name
+	end
+
+	def grade(num)
+	end
+end
