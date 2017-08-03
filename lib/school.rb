26ec7a7e51853492eca115e87ca9412f7e898d37
lauraszyms <lauraszyms@gmail.com>
2017-08-03 19:26:51 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+  attr_accessor :name, :grade, :roster
+
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+     end
+end
+
+  def grade(grade)
+     @roster[grade]
+  end
+
+  def sort
+    roster_hash = {}
+
+  @roster.each { |k, v| roster_hash[k] = v.flatten.sort }
+
+  roster_hash
+
+  end
+
+end
