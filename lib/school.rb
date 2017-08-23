d3ee729e7668072a2c52d5d742f0c206ddba7edc
Annette Maza <nettymaza@gmail.com>
2017-08-23 22:47:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -11,12 +11,14 @@ class School
   end
 
   def add_student(student_name, grade)
-    # roster[grade] ||= []
-    # roster[grade] << student_name
-    unless @roster[grade]
-      @roster[grade] = []
-    end
-    @roster[grade] << student_name
+    # if the value on the left of the equal is falsy, set it equal to the value on the right.
+    #if roster[grade] does not exist, set it to an empty array.
+    roster[grade] ||= []
+    roster[grade] << student_name
+    # unless @roster[grade]
+    #   @roster[grade] = []
+    # end
+    # @roster[grade] << student_name
   end
 
   def grade(grade_number)
