ee565dff94498a5ab5661cf06fba320fa6b8c3ce
Hunter Kleindl <hkleindl@gmail.com>
2017-09-07 21:27:17 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+
+attr_reader :name
+attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster.values_at(grade).flatten
+  end
+
+  def sort
+    roster.each {|grade, name| name.sort!}
+  end
+
+end
