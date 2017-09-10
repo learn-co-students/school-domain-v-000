c99001986778f4c7ddc0c038bc7e70b17f5c8683
Justin Tooley <jstooley823@gmail.com>
2017-09-10 06:14:18 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+    def grade(grade)
+      @roster[grade]
+    end
+    def sort
+      @roster.collect do |grade, names|
+        @roster[grade] = names.sort!
+      end
+      @roster
+    end
+
+  end
+
+end
+school = School.new("Hayside High School")
