2390496e022882d79a5d009ee42eaa414b7a3eaf
Ally Kadel <akadel01@gmail.com>
2017-09-14 14:11:40 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
 # code here!
+
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name,grade)
+    if @roster[grade] == nil
+      @roster[grade] = [student_name]
+    else
+      @roster[grade] << student_name
+    end
+  end
+
+  def roster
+    @roster
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.collect do |grade,students|
+      @roster[grade] = students.sort
+    end
+    @roster
+  end
+
+end
