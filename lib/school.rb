0372bcaf3722c06eed050e4e9396a8b48bdb2ed7
rjcolmed <rjcolmed@gmail.com>
2017-09-17 20:05:32 +0000

    add solutions to domain lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_reader :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if !@roster.has_key?(grade)
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each_value { |students| students.sort! }
+    @roster
+  end
+
+end
