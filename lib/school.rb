64320767b6b569cacefe4e207f253ced27c537c6
ndalcin <dalcin.nicole@gmail.com>
2017-09-18 10:23:18 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+
+attr_reader :name, :roster
+
+  def initialize(name)
+    @name= name
+    @roster = {}
+  end
+
+
+  def add_student(student_name, student_grade)
+    if !roster[student_grade]
+      roster[student_grade] = []
+    end # ||=
+    roster[student_grade] << student_name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.collect {|grade, array| array.sort!}
+    roster
+  end
+
+
+end
