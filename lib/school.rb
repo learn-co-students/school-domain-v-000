9d63d919d2add2820dad58bfb192ea66e78facdd
Sophie Chang <soapycee@icloud.com>
2017-09-19 22:06:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+
+class School
+  attr_accessor :roster, :student_name, :school_name
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if @roster[grade]
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |k, v|
+      v.sort!
+    end
+  end
+
+end
