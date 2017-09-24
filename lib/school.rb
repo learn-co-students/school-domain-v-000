c9c53acafb388d96d032953b67b0e83c22dca3be
Susan Kum <kum.susan@gmail.com>
2017-09-24 05:16:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    self.roster = {}
+  end
+
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+
+  def grade(grade)
+    roster.values_at(grade).flatten
+  end
+
+  def sort
+    roster.map{|grade, student| [grade,student.sort]}.to_h
+  end
+
+end
