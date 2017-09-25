126ba6809232487c590d0d9d3da00f8f8a83bd30
alpbourne <alpbourne@gmail.com>
2017-09-25 20:43:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] == nil
+       @roster[grade] = []
+       @roster[grade] << name
+    else
+       @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.map do |grade, name|
+      @roster[grade] = name.sort
+    end
+    @roster
+  end
+
+end
