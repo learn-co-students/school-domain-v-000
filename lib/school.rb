3d57d8c88a1234dbe79448b19f0386a8497eeb9f
mevanstron <michael@michaelgregoryevans.com>
2017-09-25 19:54:40 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+  attr_writer :roster, :add_student, :grade
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if !@roster[grade]
+      @roster[grade] = [name]
+    else
+      @roster[grade] << name
+    end
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+    @roster.each{|grade, values| @roster[grade] = values.sort}
+  end
+
+end
