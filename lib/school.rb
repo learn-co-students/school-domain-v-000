1c67ceac84f85af0f458c51bfe5193f1813fd103
Adam Tropp <adt6261@gmail.com>
2017-09-27 19:54:01 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+class School
+
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+  end
+
+def name
+  @name
+end
+
+  def roster
+    @roster
+  end
+
+  attr_accessor :student_name, :grade
+
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+    @roster[@grade] = []
+    @roster[@grade] << @student_name
+  end
+
+  def grade(grade)
+    puts "#{@roster[@grade]}"
+  end
+end
