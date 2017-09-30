f3d0d495ceba9020ddc574f8daae91bad83b8c1b
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-30 00:02:03 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+class School
+ attr_accessor :roster, :name
+
+  def initialize(roster={}, name)
+    @name = name
+    @roster = roster
+  end
+
+  def add_student(student, grade)
+    roster[grade]= []
+    roster[grade] << student
+  end
+end
