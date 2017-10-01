ebd4dc0948fa2bd82329f674c5b3ab18a20df994
Connie Hyman <constance.hyman@mchsi.com>
2017-10-01 04:11:10 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School
+  attr_accessor :student, :grade
+
+
+
+  def initialize(name)
+    @name = name
+
+  end
+
+  def roster
+    roster = {}
+    
+  end
+
+  def add_student
+
+
+end
