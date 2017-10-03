e03d4f717515a13c8bbf03f3d85f392a3b9862be
Melissa Rudick <mel.madrugada@gmail.com>
2017-10-03 15:45:25 +0000

    questions added

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,7 @@ class School
     # the example roster: {9: ['john', 'bob'], 10: ['melissa', 'enoch']}
     # first, is the grade a key? how do we get that key a value if there is no value (starting value)?
     @roster[grade] ||= [] # what is this again? or equals?
-    @roster[grade] << name # expects a hash of grade => name
+    @roster[grade] << name # expects a hash of grade => name # if this was an empty array, wouldn't you also need the grade key?
   end
 
 
@@ -23,7 +23,7 @@ class School
   def sort
     sorted_students = {}
     @roster.each do |grade, name|
-      sorted_students[grade] = name.sort
+      sorted_students[grade] = name.sort # how are we sure this will produce an "array" of names?
     end
     sorted_students# expects new hash of grade => array of names
   end
