4d7009493ced0fa890bfc68eaca03138f7d7a61a
Kristen Peck <kpeck71@gmail.com>
2017-10-03 00:21:24 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  attr_accessor :school, :roster
+
+  ROSTER = {[]}
+
+  def initialize(school,roster)
+    @school = school
+    #BRANDS.push(brand) unless BRANDS.include?(brand)
+  end
+
+
+  def add_student(grade,name)
+    roster[grade] << name
+    # self.condition = "new"
+    # puts "Your shoe is as good as new!"
+  end
+
+end
+
+end
