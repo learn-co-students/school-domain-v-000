21967143135201cbd3449de7c348d411527d96df
github username <wigmore3.14@gmail.com>
2017-10-18 10:29:33 -0700

    School Doman - Done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_accessor :name
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster.values_at(grade).flatten
+  end
+
+  def sort
+    @roster.each { |grade, name| name.sort! }
+  end
+end
