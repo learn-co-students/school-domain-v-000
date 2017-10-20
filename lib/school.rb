71cbf8c6e433e314751dd5bb8648b6b063fe44a5
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-10-20 15:56:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] = [] if !@roster.key?(grade)
+    @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each{|grade, students| students.sort!}
+    return @roster
+  end
+end
