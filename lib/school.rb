04bd7328bd9a99268715d52d06c0b1c07486cecd
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-20 16:45:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+require 'pry'
+class School
+
+  def initialize(test)
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = []
+    @roster[grade] << name
+  
+
+  end
+end
