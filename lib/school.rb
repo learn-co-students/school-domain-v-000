0cd4f6ad60786952c380087f9a65f45a356ceceb
Kevin Huang <yellow.khaos@gmail.com>
2017-10-22 23:06:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade]= []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    if @roster.keys.include?(grade)
+      @roster[grade]
+    else
+      puts "No students found in the grade"
+    end
+  end
+
+  def sort
+    array = @roster.sort
+    @roster = {}
+    array.each do |grade|
+      @roster[grade[0]]=grade[1].sort
+    end
+    @roster
+  end
+
+end
