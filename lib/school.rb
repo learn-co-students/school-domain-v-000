897e28fa4476829c9b4691b763fac1c98183516a
Christopher DiStasio <cadistasio@gmail.com>
2017-10-23 17:16:43 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,16 @@
 class School
+  attr_accessor :roster, :name, :grade
+
 
 def initialize(school)
   @school = school
   roster = {}
-  roster[new_key] = []
+  student_with_grade = []
 end
 
 def add_student(name, grade)
+  roster[student_with_grade] << name
+  roster[student_with_grade] << grade
 end
 
 end
