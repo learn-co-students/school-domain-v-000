190a9d372c2feec9081ba8f2f2e89675d725c085
Emily Zhao <tong.zhao.92@gmail.com>
2017-10-26 22:34:53 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+require 'pry'
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+
+  def roster
+    @roster
+  end
+
+  def add_student (student,grade)
+    if @roster.keys.include?(grade)
+      @roster[grade]<<student
+    else
+      @roster[grade] = []
+      @roster[grade]<<student
+    end
+  end
+
+  def grade(grade_number)
+    @roster[grade_number]
+  end
+
+  def sort
+    @roster.each do |grade,students_array|
+      students_array.sort!
+    end
+  end
+
+
+end
