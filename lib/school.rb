7b55dda1354270a2f2e0996c493458541a4fc170
Rebecca Tong <rtong@buffalo.edu>
2017-10-31 19:15:26 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+
+class School
+attr_accessor :roster
+
+def initialize(school)
+  @school = school
+  @roster = {}
+end
+
+def add_student(name,grade)
+  # {10:[John,Frank]}
+  if !roster[grade]
+    roster[grade]= []
+  end
+  roster[grade] << name
+end
+
+def grade(grade)
+  return roster[grade]
+end
+
+def sort
+  @roster.collect do |grade,name|
+    roster[grade] = name.sort
+    end
+    return @roster
+  end
+
+end
