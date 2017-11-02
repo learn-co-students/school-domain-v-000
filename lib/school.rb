0d2bf404b7f3c86bd43d7ef0a8c71bc17ac09359
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-02 01:55:09 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+
+    @roster = {}
+
+  end
+
+  def add_student(student, grade)
+    if @roster.include?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+  end
+
+  def sort
+  end
+
+end
