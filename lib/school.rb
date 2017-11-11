493f4d6a3001129bbb6cc4e27cce0b3984f08067
charityyoungblood <youngblood.charity@gmail.com>
2017-11-10 17:01:32 -0800

    organized code; placed roster instance variable in initialize method

--- lib/school.rb
+++ lib/school.rb
@@ -1,35 +1,32 @@
 # code here!
 require 'pry'
 
-ROSTER = []
-
 # create School class 
 class School 
 # initialize method for roster
   def initialize(school_name) 
     @school = school_name
+    @roster = {}
   end 
   
   def school
     @school
-  end  
+  end 
   
-  def roster=(school_roster)
-    @roster = school_roster
-  end
-  
-  def roster(student_name, student_grade)
-    ROSTER << student_name
-    ROSTER << student_grade
+  def roster
+    @roster
   end
   
   def add_student(student_name, student_grade)
   # the add_student method is taking key value pairs 
-  # student name - key 
-  # student grade - value 
-   student_name: student_grade
+  # student grade - key
+  # student name - value
+
+
   end  
   
 end 
 # 
 
+
+# {10 => ["AC Slater"]}
