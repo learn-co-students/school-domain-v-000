f642b96689daddd245be8b500f857e9713310498
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2017-11-12 11:43:19 -0500

    implemented required School functionality

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if !@roster.has_key?(grade)
+      roster[grade] = []
+    end
+
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    if !@roster.has_key?(grade)
+      return []
+    end
+
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each_value do |grade|
+      grade.sort!
+    end
+
+    @roster
+  end
+end
