7805062e2195fdbd51e08aa4ad1255c52af57dcd
Charity <Youngblood.charity@gmail.com>
2017-11-13 12:29:42 -0800

    added roster method to school.rb

--- lib/school.rb
+++ lib/school.rb
@@ -13,6 +13,10 @@ class School
     @school
   end 
   
+  def roster 
+    @roster
+  end
+  
   def add_student(student_name, student_grade)
   # the add_student method is taking key value pairs 
   # student grade - key
