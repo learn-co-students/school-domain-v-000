35313c8b213737531bb6941ab2642623b2aa4fb4
Liz Burton <liz.burton147@gmail.com>
2017-11-27 17:03:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -5,12 +5,23 @@ class School
 
   def initialize(school_name)
     @school_name = school_name
+#    @roster = Hash.new { | grade, name | grade[name] = Array.new }
     @roster = {}
   end
 
-  def add_student(student_name, grade)
-    @student_name = student_name
-    @student_grade = student_grade
-    @roster 
+  def add_student(student_name, student_grade)
+    if !@roster.keys.include?(student_grade)
+      @roster[student_grade] = []
+    end
+      @roster[student_grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster[grade].sort { | name1, name2 | name1 <=> name2 }
+    @roster
   end
 end
