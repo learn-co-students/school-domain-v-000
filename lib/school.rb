0653983c7ee6c7d1a9583c302ad278b8dd978e5b
Frank McCallion <keith@sensortech.com>
2017-11-27 14:32:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
-# code here!
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+
+    if @roster.key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+
+  end
+
+  def grade(grade)
+
+    @roster[grade]
+
+  end
+
+  def sort
+
+    @roster.each do |grade, students|
+
+      @roster[grade] = students.sort
+      
+    end
+
+  end
+
+end
