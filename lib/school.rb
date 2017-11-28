072ca24388f59f6a5172864866610de5fac71656
Liz Burton <liz.burton147@gmail.com>
2017-11-28 22:43:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,11 @@
+require 'pry'
 # code here!
 class School
   attr_accessor :student_name, :student_grade
   attr_reader :roster, :name
 
-  def initialize(school_name)
-    @school_name = school_name
+  def initialize(name)
+    @name = name
 #    @roster = Hash.new { | grade, name | grade[name] = Array.new }
     @roster = {}
   end
@@ -21,8 +22,22 @@ class School
   end
 
   def sort
-    @roster.map do | grade, names |
+    @roster.each do | grade, names |
       @roster[grade] = names.sort
     end
   end
 end
+
+# school = School.new("Bayside High School")
+# school.add_student("Homer Simpson", 9)
+# school.add_student("Bart Simpson", 9)
+# school.add_student("Avi Flombaum", 10)
+# school.add_student("Jeff Baird", 10)
+# school.add_student("Blake Johnson", 7)
+# school.add_student("Jack Bauer", 7)
+# school.add_student("Claudia Jean Cregg", 7)
+# school.add_student("Jane Villanueva", 7)
+# school.add_student("Maura Pfefferman", 7)
+# school.add_student("Jackie Peyton", 7)
+# school.add_student("Red Reznikov", 7)
+# binding.pry
