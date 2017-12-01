d4013e3df930ca1c21dadb2b8e9bd2d75f27914d
Hannah Carnes <hecarnes@gmail.com>
2017-11-30 23:10:20 -0500

    implemented school

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade_number)
+    @roster[grade_number] = grade(grade_number) + [student_name]
+  end
+
+  def grade(grade_number)
+    @roster[grade_number] || []
+  end
+
+  def sort
+    @roster.map do |grade_number, student_names|
+      [grade_number, student_names.sort]
+    end.to_h
+  end
+end
