b43725b0f4e9267accb18d87fa762940a5ff178e
Matthew Hunger <matt.hunger@gmail.com>
2017-12-02 18:23:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name=name
+    @roster = {}
+  end
+
+  def add_student(student, grade_of_student)
+    if @roster[grade_of_student] == nil
+      @roster[grade_of_student] = []
+    end
+    @roster[grade_of_student] << student
+  end
+
+  def grade(grade_roster)
+    return roster[grade_roster]
+  end
+
+  def sort
+    @roster.each do |r|
+      array = r[1]
+      array.sort_by! {|b| b.downcase}
+    end
+    return @roster
+  end
+end
