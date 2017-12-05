08f7cac538cc2e35fa67ad896b62c9ed309b7bc9
Richard Schaubeck <richard.schaubeck@gmail.com>
2017-12-05 01:20:31 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, student|
+      student.sort!
+    end
+  end
+
+end
