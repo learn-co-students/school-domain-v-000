821c724b72d7c1f957befe1dcfa27370cf0259ee
Jennifer Stoos <jstoos@bitlaw.com>
2017-12-07 22:13:17 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require "pry"
+
+class School
+  attr_accessor :roster, :name, :grade
+
+  # def roster
+  #   @roster
+  # end
+  # def roster=(roster)
+  #   @roster = roster
+  # end
+
+  def initialize(school)
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if roster[grade] == nil
+      roster[grade] = []
+    end
+    roster[grade] << name
+  end
+
+  def grade(year)
+    roster[year]
+  end
+
+  def sort
+    roster.each do |year, data|
+        data.sort!
+    end
+    roster
+  end
+
+end
