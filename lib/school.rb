be8ea736b5ef4eb61fc99fcb62d3b3d8cf501834
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2017-12-15 22:10:50 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :roster
+
+    def initialize(name)
+      @name = name
+      @roster = {}
+      end
+    def add_student (student, grade)
+      unless roster.has_key?(grade)
+        @roster[grade] = []
+      end
+      @roster[grade] << student
+    end
+    def grade (grade)
+      @roster[grade]
+    end
+    def sort
+      sorted = {}
+        @roster.each do |grade, students|
+          sorted[grade] =students.sort
+        end
+        sorted
+    end
+end
