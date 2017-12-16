e7789b14b3e649be264137f97a737dce1b80b644
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2017-12-15 16:21:41 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+    attr_accessor :grade, :roster
+    attr_reader :name
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        !roster.include?(grade) ? @roster[grade] = [] : false
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        return @roster[grade]
+    end
+
+    def sort
+        @roster.each {|grade, name| name.sort!}
+    end
+end
