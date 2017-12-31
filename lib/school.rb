5bc24b2fd40d5233ad77442d5ad492e4f443077f
funemployeddad <funemployeddad@gmail.com>
2017-12-31 11:49:32 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+class School
+attr_accessor :name, :roster, :grade
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    
+  end
+
+end
