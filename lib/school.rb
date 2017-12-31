8fbbd729a3ff07937012859966272558ae53d579
funemployeddad <funemployeddad@gmail.com>
2017-12-31 13:12:30 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,9 @@ attr_accessor :name, :roster, :grade
   end
 
   def add_student(student_name, grade)
-    
+    roster[new_student] = []
+    roster[new_student] << student_name
+    roster[new_student] << grade
   end
 
 end
