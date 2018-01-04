61e35cfa449f4459d51937c5bddb99a7e93dd055
kim alban <kimalbans@yahoo.com>
2018-01-04 03:39:21 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+class School
+attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {} #add roster here
+ end
+
+  def add_student(name, grade)
+    if @roster.has_key?(grade) #need to ask roster to see if there is a grade
+      @roster[grade] << name
+    else
+     @roster[grade] = [name] #this is if key of that value
+   end
+  end
+
+  def grade(grade)
+    self.roster[grade] #calling self.roster which returns contents of grade
+    # @roster[grade]
+  end
+
+  def sort
+    self.roster.each do |grade, students|
+      students.sort!
+  end
+end
+
+end
