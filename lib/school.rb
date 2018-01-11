21961617474045ee90a714e14d0405e759c8859c
Andrew Hong <hongandrew16@gmail.com>
2018-01-11 01:15:41 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(school_name)
+    @name = school_name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.collect do |key, value|
+      @roster[key].sort!
+    end
+
+    @roster
+  end
+
+end
