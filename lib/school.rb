8bb93dcec0dc288d24b0fa8e69a41bfeab28e8e7
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-12 13:29:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def name
+    @name
+  end
+
+
+  def add_student(name, grade)
+    if @roster[grade] = nil
+      @roster[grade] = [name]
+    else
+      @roster[geade] << [name]
+    end
+  end
+
+  def roster
+    @roster
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+end
