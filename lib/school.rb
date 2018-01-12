4223d82a6c00c9efc8c05b13812f3be0d4c776a6
Lindsey Trainor <ltrainor38@gmail.com>
2018-01-12 03:57:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School 
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def roster
+    @roster 
+  end 
+
+  def add_student(name, grade)
+  if @roster[grade] == nil 
+    @roster[grade] = []
+  end 
+    @roster[grade] << name 
+  end 
+
+
+def sort 
+ @roster.update(@roster) {|key, value| value.sort {|a,b| a[0] <=> b[0]}}
+end 
+
+def grade(num)
+  @roster[num]
+end 
+
+end 
