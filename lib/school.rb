557effd9c9a12bba4b0e36b83e2277cc837decee
Daiane Bushey <daianebushey@gmail.com>
2018-01-12 19:08:10 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  #attr_accessor
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+  end
+
+  def roster
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] = []
+    @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    #returns an array with all students with that particular grade
+  end
+
+end
