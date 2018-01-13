d611790129c5fe3fe91eef9bfca877e71c56c0c6
kanndide <kanndide@gmail.com>
2018-01-13 15:52:41 +0300

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,45 @@
-# code here!
+require 'pry'
+
+
+class School
+    
+    attr_accessor :roster
+    attr_reader :name
+    
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end 
+    
+    
+    def add_student(student, grade)
+        if roster.include?(grade) != true
+            roster[grade] = []
+            roster[grade] << student
+        else 
+            roster[grade] << student
+        end
+    end
+    
+    def grade(arg)
+        roster[arg]
+    end
+    
+    def sort
+        sort = {}
+        roster.each do |year, data|
+            data.sort!
+        end
+        roster
+    end
+
+
+end
+
+#sort = {}
+#        roster.each do |year, data|
+#            data.sort!
+#        end
+#        roster
+#        end
