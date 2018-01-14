5ad28224bda46247260dbce5025ad0574ff8c2c4
Catherine Salazar <catcreates13@gmail.com>
2018-01-14 16:11:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+class School
+attr_accessor :name, :roster
+
+def initialize(name)
+
+  @name = name
+  @roster = {}
+
+end
+
+def add_student(student_name, grade)
+  if !@roster[grade]
+    @roster[grade] = []
+  end
+  @roster[grade] << student_name
+end
+
+def grade(grade_input)
+  @roster[grade_input]
+
+end
+
+def sort
+  @roster_new = {}
+  @roster.each do |key, value|
+    @roster_new[key] = value.sort
+end
+@roster_new
+end
+end
+
+
+
+{
+  grade: []
+}
