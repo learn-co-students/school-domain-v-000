8bc5b3d77d6095f54a3aa3719da7927c1db18c95
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-16 20:00:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  # this method should arrange the students in each grade by alphabetical order
+  def sort
+    sorted = {}
+    roster.each {|grade, students| sorted[grade] = students.sort}
+    sorted
+  end
+end
