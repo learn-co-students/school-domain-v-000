a55bd91803935e987ad71de29c73c24ffd8310bd
jacobsilver2 <jacobsilver2@mac.com>
2018-01-15 22:34:37 -0500

    did the work

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+class School
+   attr_reader :roster, :name, :grade
+   
+   def initialize(name)
+      @name = name
+      @roster = {}
+   end
+
+   def add_student(name, grade)
+      @name = name
+      @grade = grade
+
+      @roster[@grade] = [] unless @roster.has_key?(@grade)
+      @roster[@grade] << @name
+
+   end
+
+   def grade(grade)
+      @roster[grade]
+   end
+
+   def sort
+      @roster.each do |k,v|
+         @roster[k] = v.sort
+      end
+
+   end
+
+end
