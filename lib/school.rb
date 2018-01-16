5a6838190caca51fdd4768bc6a61629b5edaf8c2
Ingrid Wong <yw340@georgetown.edu>
2018-01-16 16:36:27 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster ={}
+  end
+
+
+  def add_student(name, grade)
+    self.roster[grade] ||= []
+    self.roster[grade] << name
+  end
+
+  def grade(gr)
+    self.roster[gr]
+  end
+
+  def sort
+    self.roster.each do |grade, names|
+      names = names.sort do |a, b|
+        a <=> b
+      end
+      self.roster[grade] = names
+      self.roster
+    end
+  end
+end
