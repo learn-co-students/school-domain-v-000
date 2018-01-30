9eb46b3626f4ce16175ba79d300707e175acea73
Katrina Brinson <kbrinso@gmail.com>
2018-01-30 18:15:56 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,31 +1,34 @@
-# code here!
 class School
-  attr_accessor :name
-  attr_writer :roster,:grade
-  
   ROSTER = {}
   
-  def initialize(name)
-    @name = name
+  def initialize(school)
+    @school = school
+  end
+  
+  def roster=(roster)
     @roster = roster
   end
   
-  def add_student=(student_name,grade)
-    if ROSTER[grade] == nil
-      ROSTER[grade] = []
-      ROSTER[grade] << student_name
+  def roster
+    ROSTER
+  end
+  
+  def add_student(student_name,grade)
+    @student_name = student_name
+    @grade = grade
+    if ROSTER[@grade].kind_of?(Array)
+      ROSTER[@grade] << @student_name
     else 
-      ROSTER[grade] << student_name
+      ROSTER[@grade] = []
+      ROSTER[@grade] << @student_name
     end
   end
   
-  def roster(roster)
-    @roster = roster
-    roster = ROSTER
+  def grade(grade)
+    ROSTER[grade]
   end
   
-  def grade(grade)
-    @grade = grade
-    ROSTER[grade]
+  def sort
+    ROSTER.sort_by {|_key, value| value}.to_h
   end
 end
