d9ad9fffbdf990475caa671cf5b9639983fd9e7d
Jayon Kim <jpkim921@gmail.com>
2018-01-31 03:19:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+# require 'pry'
+
+class School
+attr_accessor :roster, :student
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+
+    if @roster.keys.include?(grade)
+      @roster[grade] << student_name
+      # binding.pry
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |k,v|
+      @roster[k] = v.sort!
+    end
+  end
+
+end
