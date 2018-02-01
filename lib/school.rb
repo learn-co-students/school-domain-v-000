b4f40cc568a0b7a38106a8f99fc509717fcec49e
Brennan Heisler <learn@heislercreative.com>
2018-02-01 03:07:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,8 @@ class School
   end
   
   def add_student(name, grade)
-    roster[grade] = roster[grade] || [ ]
+    # roster[grade] = roster[grade] || [ ]
+    roster[grade] ||= [ ]
     roster[grade] << name
   end
   
