76f5e79985996697d539c38be297c6d634e7ecf2
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-06 03:04:39 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster.key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    @roster.each{|grade, grade_array| sorted[grade] = grade_array.sort}
+    sorted
+  end
+end
