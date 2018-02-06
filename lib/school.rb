5092763d5ca63b80baf5f2e6276ecdf158652aa6
Rebecca Strauss <rstrauss127@gmail.com>
2018-02-06 18:59:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+
+  def add_student(student, grade)
+
+    if @roster.has_key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = Array.new
+      @roster[grade] << student
+    end
+
+  end
+
+  def roster #reader for roster
+    @roster
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end
