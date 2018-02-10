3246f97eb9bb36eb4e18d54e9043af1c7e81aea4
Chely Ho <adept_inept@yahoo.com>
2018-02-10 19:04:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+class School
+  attr_accessor
+  attr_reader :name, :roster
+
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)  # roster = {10 =>["AC Slater"]}
+    @roster[grade] = [] if @roster[grade].nil?
+    @roster[grade] << student
+  }
+
+  end
+end
