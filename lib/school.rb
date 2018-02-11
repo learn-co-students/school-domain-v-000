066624cf0e3c38ffac0c44492dfb1aa79c95765f
Eric Johnson <0216johnsoneric@gmail.com>
2018-02-11 19:59:27 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
 # code here!
+class School
+  attr_accessor :roster, :name
+
+  def initialize(name)
+    @name = name
+    @roster = []
+  end
+
+  # def roster
+  #   @roster
+  # end
+
+  def add_student=(name)
+    @name = name
+  end
+
+  # def student
+  #   @student
+  # end
+
+
+  #   if @roster[grade] != nil
+  #      @roster[grade] << name
+  #   else
+  #     @roster[grade] = [name]
+  #   end
+  # end
+  #
+  # def grade(grade)
+  #   def grade(grade)
+  # end
+  #
+  # def sort
+  #   @roster.each do |key, value|
+  #     value.sort!
+  #   end
+  # end
+end
