a5f2dbd89fae1790d7002ceb2daa495401584b8f
Harley Rayner <harleymteden@gmail.com>
2018-02-13 17:42:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -12,8 +12,19 @@ class School
 
   def add_student(student_name, student_grade)
     @roster[student_grade] = [] unless @roster.keys.include?(student_grade)
-    @roster[student_grade] << student_name  
+    @roster[student_grade] << student_name
     # @roster[student_grade] = []
+  end
+
+  def grade(student_grade)
+    @roster[student_grade]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      name.sort!
+    # binding.pry
+    end
   end
 
 end
