fb4a9d17b8a325af81ebe4f581a0a1fa2ca9d675
Jay Bednar <ironviralmedia@gmail.com>
2018-02-13 02:45:05 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,11 @@
-require 'pry'
-
 # code here!
 class School
   attr_reader :roster
-  # attr_reader :add_student,
 
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
   end
-
-  # def roster
-  #   @roster = {}
-  # end
 
   def add_student(student_name, student_grade)
     if @roster.has_key?(student_grade)
@@ -21,7 +14,6 @@ class School
       @roster[student_grade] = []
       @roster[student_grade] << student_name
     end
-    #binding.pry
   end
 
   def grade(grade_num)
