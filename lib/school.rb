8e02323fac0728432283e9ff4dace51bd07d7c68
Harleigh Abel <harleighabel@gmail.com>
2018-02-14 22:43:27 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,28 +1,28 @@
+require 'pry'
+
 class School
   attr_accessor :roster
   attr_reader :name, :grade
-
-  def initialize(title)
-    @title = title
-    @roster = {}
+  
+  def initialize(name)
+    @name= name
+    @roster = {} 
   end
-
+  
+  
   def add_student(student, grade)
-    if @roster.include? (grade)
-      @roster[grade] << student
-    else
+    if @roster[grade].nil?
       @roster[grade] = []
-      @roster[grade] << student
     end
-  end
+      @roster[grade] << student
+  end  
+  
   def grade(grade)
     @roster[grade]
-  end
-
-  def sort
-    @roster.each do |k, v|
-        v.sort!
-    end
-  end
-    
-end
+  end 
+  
+  def sort 
+    @roster.sort
+    @roster.each {|grade, student_array| student_array.sort!}
+  end  
+end  
