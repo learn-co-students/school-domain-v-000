9eececdb0e1472eabc0f555fe58d68563b7855ac
Marin Cabac <marin.cabac@gmail.com>
2018-02-19 15:21:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+
+class School
+  attr_accessor :school, :roster
+  roster={}
+######################################
+  def initialize(school_n)
+    @school=school_n
+    @roster = {}
+     end
+######################################
+def add_student(n,g)
+ @roster[g] ||= []
+  @roster[g]<<n
+end
+######################################
+def grade(g)
+  @roster[g]
+end
+######################################
+def sort
+@roster.each{|k,v|v.sort!}
+end 
+######################################
+
+
+end
