d9c4eb0cd6e1ec874f126a7ea0c135749cea2937
Aspen James <aspenjames@tqca.org>
2018-03-06 22:05:02 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require'pry'
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] if @roster[grade].nil?
+    @roster[grade] << name
+  end
+
+  def grade(n)
+    @roster[n]
+  end
+
+  def sort
+    @roster.sort
+    @roster.each{ |grade, name| name.sort! }
+    @roster
+  end
+end
