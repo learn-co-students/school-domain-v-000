1064bc4c2896078e174a4567dce001a9bb599d19
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-15 20:21:21 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_reader :name
+  attr_accessor :roster, :add_student
+  
+  
+  def initialize(name)
+    @name 
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+   @roster[grade] ||= []  
+    @roster[grade] << student
+  end
+  
+  def grade(grade)
+   @roster[grade]
+  end
+  
+  def sort
+    @roster.map do |grade, students|
+      students.sort!
+    end
+    @roster
+  end
+  
+end
