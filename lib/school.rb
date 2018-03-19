c542721a8570eceb00bf9440bf46a3fd05215fac
Parul Gupta <parulguptasmail@gmail.com>
2018-03-19 19:13:07 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require 'pry'
+class School 
+  
+  def initialize(name)
+    @name = name
+    @roster = Hash.new {|h,k| h[k] = [] }
+  end
+  
+  def name
+    @name
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(student_name, grade)
+    @roster[grade] << student_name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort()
+    h = @roster.sort.to_h
+    
+    h.each do |key,value|
+      h[key].sort!
+    end
+    h
+  end
+ end
