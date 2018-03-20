257936b9b460ed0e41cf430a9502cb2f00e8a170
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-03-20 16:52:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,38 +1,41 @@
 class School
-
-  def initialize(school)
-    @school_name = school
+  
+  attr_accessor :name
+  
+  def initialize(school_name)
+    @name = school_name
     @roster = { }
   end
-
-
-  def school
-    @school_name
-  end
-
+  
+  # Add one or more students to a specific grade
+  def add_student(student_name, grade)
+    
+    if @roster.include?(grade) == true
+      @roster[grade] << student_name
+    else
+      @roster[grade] = [ ]
+      @roster[grade] << student_name
+    end
+    
+  end # add_student
+  
+  
+  
+  # able to retrieve students from grade
+  def grade(grade)
+    @roster[grade]
+  end # grade
+  
+  
+  # Sorts the students
+  def sort
+    @roster.each do |grade, students|
+      @roster[grade] = students.sort
+    end
+  end # sort
+  
   def roster
     @roster
-  end
-
-  def add_student(student, grade)
-    if @roster.include?(grade) == false
-      @roster[grade] = []
-      @roster[grade] << student
-    else
-      @roster[grade] << student
-    end
-
-    @roster
-  end
-
-  def grade(student_grade)
-    @roster[student_grade]
-  end
-
-  def sort
-    @roster.each_value {|grade| grade.sort!}
-
-    @roster
-  end
-
-end
+  end # roster
+  
+end #=> ends class School
