af7990feb1cd7c9b450409df63655f44125568f6
Victoria Meng <nights1ststar@protonmail.com>
2018-03-20 22:04:47 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+  attr_accessor :roster
+
+  def initialize(school)
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    roster[grade] = [] if !roster.has_key?(grade)
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each { |grade, students| students.sort! }
+  end
+
+end
