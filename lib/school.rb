05e6cdd0ea62aced23f31cfc657b5afbda33619b
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-03-23 16:04:07 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
-# code here!
+require 'pry'
+
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def name
+    @name
+  end
+  
+  def roster=(roster)
+    @roster = roster
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    #if roster[grade] exists
+    if roster[grade]
+      #add name to array
+      roster[grade] << name
+    else
+      #new key,value pair
+      roster[grade] = []
+      roster[grade] << name
+    end
+    roster
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def sort
+    roster.each {|grade, names| names.sort!}
+    end
+  
+end
