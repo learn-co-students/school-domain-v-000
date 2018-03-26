79a8d4943a7aa3adcb13161af9309d390dc5733e
Erica Murdock-Waters <ericahpa@gmail.com>
2018-03-26 14:41:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+# school.rb
+class School # School class referredto as a 'model' in the domain model
+  attr_accessor = :name, :roster
+#  attr_reader = :roster
+
+  def initialize(name, roster = {})
+    @roster = roster #create empty hash roster upon initialization
+    @name = name
+
+  end
+
+  def roster
+    @roster
+  end
+
+  # @roster = keys - grade levels, value - array of students names
+  def add_student(student, grade)
+    if @roster.include?(grade) #if grade key already exists in hash
+        @roster[grade] << student # add student to that grade
+      else @roster[grade] = [] #else create grade key in roster hash
+        @roster[grade] << student # and then add student
+      end
+  end
+
+  def grade(grade_key)
+    @roster[grade_key]
+  end
+
+  def sort
+    @roster.map do |grade_key, students|
+      students.sort!
+    end
+    @roster
+  end
+end
