661819aa55daf338fbdbadc41ac62c39d7c52c7f
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-26 22:34:29 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+
+class School
+  attr_reader :school_name, :school_roster
+
+
+
+  def initialize(school_name)
+    @school_name = school_name
+    @school_roster = {}
+  end
+
+  def add_student(s_name)
+    @s_name = s_name
+    @school_roster << s_name
+  end
+
+end
