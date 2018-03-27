b5c57c1e02ecb5a3c92e975f2d79ab7a20321371
Vanessa Toro <vanetoro84@gmail.com>
2018-03-27 18:57:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,16 @@
-# code here!
 
 class School
-  NEW  = []
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student, grade)
+    roster[grade] = []
+    roster[grade] << student
+  end
+  
+
 end
