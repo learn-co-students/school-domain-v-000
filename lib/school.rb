4ed9ade3225ad58eb5804ff88fe8130da28f6624
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-04 23:21:09 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
+require "pry"
+
+class School 
+  attr_accessor :name
+  attr_reader :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    if roster.include?(grade)
+      roster[grade] << student
+    else
+      roster[grade] = []
+      roster[grade] << student
+    end
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def sort
+    roster.each do |student|
+      student[1].sort!
+    end
+  end
+ 
+end
+
+
 # code here!
