26cb8d92c7b20e0ffe3e8d9c2d545ea0c501afbb
Jonathan Dust <d24dust@hotmail.com>
2018-04-06 18:31:07 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  attr_reader :roster, :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    self.roster[grade] = [] if self.roster[grade] == nil
+    self.roster[grade] << name
+  end
+
+  def grade(grade_num)
+    return self.roster[grade_num]
+  end
+
+  def sort
+    sorted = Hash[self.roster.sort]
+    sorted.each {|g, n| sorted[g] = n.sort}
+    return sorted
+  end
+
+end
