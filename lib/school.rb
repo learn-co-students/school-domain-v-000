13a7678ac4a31c28d96be2f728a44fdbf6ef2663
Joanne Lee <vernapii@gmail.com>
2018-04-06 22:03:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+
+
+
+def add_student(names, grade)
+  @roster[grade] ||= []
+  @roster[grade] << names
+end
+
+def grade(num)
+  result = []
+  result << @roster.values_at(num)
+  result.flatten.compact
+end
+
+def sort
+  resulting = {}
+  @roster.each {|k,v| resulting[k] =  v.sort}
+
+
+resulting
+end
+end
