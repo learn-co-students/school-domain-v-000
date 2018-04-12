9d10ffaca08832280c1e7925dcec5051692add66
Isaac Villicana <villicana.i@gmail.com>
2018-04-12 15:37:31 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_reader :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def add_student(student, grade)
+      @roster[grade] ||= []
+      @roster[grade] << student
+  end
+
+  def grade(level)
+    @roster[level]
+  end
+
+  def sort
+    @roster.collect do |grade, students|
+      students.sort!
+    end
+    @roster
+  end
+end
