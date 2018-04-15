2a5858d5c4f9f3f7dc476aab6a44b647a85420aa
Everett <everett.bowline@gmail.com>
2018-04-15 20:56:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :student, :roster
+  
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def sort
+  roster.each do |k, v|
+    roster[k] = v.sort{ |a,b| a[0]<=>b[0] }
+  end
+  end
+  
+end
