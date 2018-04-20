637341a42463fb79b23c5d55689deffda30ce6bf
Joseph Parker <jparker010190@gmail.com>
2018-04-20 00:54:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  def roster
+  @roster
+  end
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end
