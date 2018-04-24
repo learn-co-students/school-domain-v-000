d63ec86659b4177cefe828346fe1889d0108ea1b
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-24 19:45:13 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+require "pry"
+class School
+  attr_accessor :name, :roster, :student, :grade
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(student, grade)
+  #binding.pry
+  if @roster.keys.include?(grade)
+    @roster[grade] << student
+  else
+    @roster[grade] = []
+    @roster[grade] << student
+  end
+end
+
+
+end
