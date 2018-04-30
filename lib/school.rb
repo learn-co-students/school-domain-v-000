0fb61442ad426e1f6116279c3014e410bc470abc
Brent Bauman <brentbauman86@gmail.com>
2018-04-30 13:06:31 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+attr_accessor :roster, :name, :grade
+
+def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+end
+
+#:grade => ["student"]
+def add_student(name, grade)
+  @name = name
+  @grade = grade
+
+  roster[grade] = []
+  roster[grade] << name
+
+
+end
+# def grade(kids_grade)
+#   @grade = grade
+# kids_grade.collect do |grade|
+#   end
+# end
+
+end
