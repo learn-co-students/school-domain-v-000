9bd8767432fd3f5df6fc0302cd4d10d093024c34
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-04 17:33:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
 # code here!
+require "pry"
+class School 
+  def initialize(name, roster)
+    @name = name
+    @roster = roster 
+    roster = {}
+  end
+  
+end
+
+school = School.new("Bayside High School")
+school.roster
