57c0c4e1ac03072c2c9d165054742e495efe3246
Daksha Patel <dakshapatel100@gmail.com>
2018-05-08 14:17:15 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+require 'pry'
+class School
+binding.pry
+roster= {}
+
+def initialize(name)
+
+
+end
+
+
+def add_student(name, grade)
+
+end
+
+def grade(grade)
+  return all of the students in that grade
+
+
+end
+
+def sort
+end
+
+
+end
