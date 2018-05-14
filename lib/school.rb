bdf81b94dec39d090d5b002f1078c34673b23c17
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-14 20:46:53 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(students_name, grade)
+    if @roster[grade]
+      @roster[grade] << students_name
+    else
+      @roster[grade] ||= [students_name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.sort.to_h
+  end
+end
