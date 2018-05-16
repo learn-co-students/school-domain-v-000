034dd6321996dfbce25c3a3226a0bdffeeba1ed1
Emily Welsch <emwelsch@gmail.com>
2018-05-16 20:31:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    roster = {}
+  end
+
+  def roster
+    roster = {}
+  end
+
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+
+end
+
+
+
+# def roster
+#   roster = {
+#     # hash = {"key1" => "value1"}
+#     # "Grade 1" => ["Elizabeth","Emily","Kristina"],
+#     # "Grade 2" => ["Elizabeth","Emily","Kristina"],
+#     # "Grade 3" => ["Elizabeth","Emily","Kristina"]
+#   }
+# end
