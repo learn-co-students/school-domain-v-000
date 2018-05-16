71942ec48c4c782918a73ef6f195c5231d65bd6d
Laura Messa Travin <laura.travin@gmail.com>
2018-05-16 03:34:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+  attr_accessor :roster
+  attr_reader :school
+  def initialize(n)
+    @school = n
+    @roster = {}
+  end
+  def add_student(n,g)
+     if self.roster.empty?
+       self.roster = {g => [n]}
+     else
+         if self.roster.keys.include?(g)
+              self.roster[g] << n
+         else
+              self.roster[g] = [n]
+         end
+     end
+  end
+  def grade(g)
+    self.roster[g]
+  end
+  def sort
+    @sort = {}
+    self.roster.keys.sort.each { |chr| @sort[chr]= roster[chr].sort  }
+    return @sort
+  end
+end
