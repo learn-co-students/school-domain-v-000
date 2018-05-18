220634b58a6f7adfe53f14094167c05a6a70a9bf
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-18 03:08:19 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,15 @@
 # code here!
 
 class School 
-  def initialize(school)
-    @school = school
-    @roster = {}
-  end
-  
   attr_accessor :school, :roster
   
+  def initialize(school)
+    self.school = school
+    self.roster = {}
+  end
+  
   def add_student(name, grade)
-    if roster[grade] == nil
-      roster[grade] = [name]
-    else
-      roster[grade] << name
-    end
+    roster[grade] == nil ? roster[grade] = [name] : roster[grade] << name 
   end
   
   def grade(grade)
