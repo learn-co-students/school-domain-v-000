5957ae53406c9c91b4c4232dc91d796085e26e38
Dale Terrell <tragicid@gmail.com>
2018-05-19 01:46:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zach Morris", 9)
+school.roster
+
+hash[new_key] = []
+hash[new_key] << new_value_for_value_array
+ 
+hash
+ => {"new_key"=>["new_value_for_value_array"]} 
+ 
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
+
+school.grade(9)
+
+school.sort
