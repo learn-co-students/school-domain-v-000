edbfcab7a79bd4107a59745e359315c97d2881e7
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-05-21 21:33:50 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade].push(name)
+  end 
+  
+  def grade(grade)
+    roster.values_at(grade).flatten
+  end 
+  
+  def sort 
+    roster.each do |key, value|
+      value.sort!
+    end 
+  end 
+  
+end 
