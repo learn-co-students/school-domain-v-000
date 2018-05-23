13c3670b54c9faf2ec10047bb0eb38043356c1bf
Taylor Williams <taylor@thewilliams.net>
2018-05-23 18:40:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+  attr_accessor :name
+  attr_reader :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student(name, grades)
+    roster[grades] = [] if !roster.keys.include?(grades)
+    roster[grades] << name
+  end
+
+  def grade(grades)
+    roster[grades]
+  end
+
+  def sort
+    roster.sort
+  end
+end
