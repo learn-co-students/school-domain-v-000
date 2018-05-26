a3a2434cb53728da38132fbdf000495af08e4b23
Leslie Stevens <flylady@mail.utexas.edu>
2018-05-26 16:18:07 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+class School
+
+  attr_accessor :name, :roster
+
+
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+
+
+
+
+    def add_student(student, grade)
+      if roster.has_key?(grade) != true
+        roster[grade] = []
+        roster[grade] << student
+      else
+        roster[grade] << student
+      end
+      roster
+    end
+
+    def grade(grade)
+      array = []
+      roster[grade].each do |student|
+        array.push(student)
+      end
+      array
+    end
+
+    def sort
+      roster.each do |grade, students|
+        students.sort!
+      end
+    end
+
+end
