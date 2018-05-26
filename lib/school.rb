bcf196733fd098244842f1b7cd813d5269f13854
Natalia Castro <nat.alvz@gmail.com>
2018-05-26 22:21:08 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+attr_accessor :name, :roster, :grade
+
+
+ def initialize(name)
+   @name = name
+   @roster = {}
+end
+
+def add_student(student, grade)
+ if roster.has_key?(grade) != true
+  roster[grade] = []
+  roster[grade] << student
+else
+  roster[grade] << student
+end
+roster
+  end
+  def grade(grade_year)
+    @roster.fetch(grade_year)
+    end
+    
+    def sort 
+      roster.each { |key, value| value.sort! }
+    end
+end
