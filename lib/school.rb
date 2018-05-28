b83af0666bf78fa8393837e973c68a3b94e040dd
Matt Farmer <mrfarmer777@gmail.com>
2018-05-28 21:33:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require 'pry'
+
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name=name
+    @roster={}
+
+  end
+
+  def add_student(student,gr)
+    if !@roster[gr]
+      @roster[gr]=[]
+    end
+    @roster[gr]<<student
+  end
+
+  def grade(gr)
+    @roster[gr]
+  end
+
+  def sort
+    @roster.each do |gr,student_array|
+      student_array.sort!
+    end
+    #binding.pry
+    @roster
+  end
+end
