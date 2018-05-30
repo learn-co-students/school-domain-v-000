eacfa76ec90d81c2677f50f3e39f14ba5fddab51
Elaine Anderson <eparie@bu.edu>
2018-05-30 17:54:54 +0000

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+
+def add_student=(student_name, grade)
+  @student_name = student_name
+  @grade = grade
+end
+
+def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    sorted_hash = {}
+roster.each do |grade, students|
+sorted_hash[grade] = students.sort
+end
+sorted_hash
+  end
+
+
+end
