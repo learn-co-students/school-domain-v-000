ddbbbacf1be1e0011d7478da93b2a9d870ea8016
Leticia Molina <letimolina@gmail.com>
2018-06-03 22:21:26 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+  
+  # attr_accessor :
+  attr_reader :new, :student, :roster
+  
+  def initialize(new)
+    @new = new
+  end
+  
+ ROSTER = []
+  # def name(name)
+  #   @name = name
+  # end
+  
+  # def name
+  #   @name
+  # end
+  
+  def add_student(student, school)
+    ROSTER << student
+  end
+  
+end
