1ce090e13199dd1e31b0f70ea0fb8c4f0d4e3ffa
Peter Kim <peterckim96@gmail.com>
2018-06-04 18:44:05 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(student, age)
+    if @roster[age] != nil
+      @roster[age] << student
+    else
+      @roster[age] = []
+      @roster[age] << student
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end
