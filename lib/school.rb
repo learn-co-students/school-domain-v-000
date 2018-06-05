9c50848891faceec0954104fe0b42d1f4ce430d9
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 02:58:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+  attr_accessor :roster
+  attr_reader :name
+
+  roster = {};
+
+  def initialize(name)
+      self.roster = {};
+  end
+
+  def add_student(student_name, grade)
+    if !(roster.has_key?(grade))
+      roster[grade] = [];
+    end
+    roster[grade] << student_name;
+  end
+
+  def grade(grade)
+    roster[grade];
+  end
+
+  def sort
+    roster.map {|grade, students| roster[grade] = students.sort}
+    roster;
+  end
+end
