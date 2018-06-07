720cea759355d71399e45ad2d57277fcd89debd0
Mark Lawrence <lawrence.ml@gmail.com>
2018-06-07 01:33:10 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if !roster.has_key? grade
+      roster[grade] = []
+      roster[grade] << student
+    else
+      roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    return roster[grade]
+  end
+
+  def sort
+    roster.each do |k, v|
+      v.sort!
+    end
+  end
+end
