f3ffa849f3dacf7b880d2705d39c1d2a2e081810
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-08 21:54:56 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+# create a new class School
+class School 
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end 
+  
+  def add_student(student_name, grade)
+    unless @roster.keys.include?(grade)
+      @roster[grade] = []
+    end
+    
+    unless @roster[grade].include?(student_name)
+      @roster[grade] << student_name
+    end
+  end
+  
+  def grade(grade_number)
+    @roster[grade_number]
+  end
+  
+  def sort 
+    sorted = @roster.each_value(&:sort!)
+  end
+  
+end
