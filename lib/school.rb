9d00114ccbb1f300a195ebcf3677b3d148556938
Leticia Molina <letimolina@gmail.com>
2018-06-09 19:40:34 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,26 @@
 class School
   
   # attr_accessor :
-  attr_reader :new, :student, :roster
+  attr_reader :name, :roster
   
-  def initialize(new)
-    @new = new
+  def initialize(name)
+    @name = name
+    @roster = {}
   end
   
- ROSTER = []
-  # def name(name)
-  #   @name = name
-  # end
-  
-  # def name
-  #   @name
-  # end
-  
-  def add_student(student, school)
-    ROSTER << student
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
   end
   
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.map do |grades, students|
+      @roster[grades].sort!
+    end
+    @roster
+  end
 end
