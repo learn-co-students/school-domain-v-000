6c8e2b1b321cba7e8b1fca17f2c13d86da604861
Jason Mei <jason.mei403@gmail.com>
2018-06-14 05:12:48 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, grade)
+    @student = student
+    @grade = grade
+    if @roster[grade] == nil
+      @roster[grade] = [student]
+    else
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each_value do |students|
+      students.sort!
+    end
+  end
+end
