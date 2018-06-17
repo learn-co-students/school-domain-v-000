8059bb179ad75cccbc9afe2c6321cd56dc2b40b6
Adam Barton <adam.barton333@gmail.com>
2018-06-17 19:38:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,8 @@ class School
   end
 
   def add_student(name, grade)
-    @roster[grade] = [] unless @roster.include?(grade)
+    @roster[grade] ||= []  
+    #   @roster[grade] = [] unless @roster.include?(grade)
     @roster[grade] << name
   end
   
