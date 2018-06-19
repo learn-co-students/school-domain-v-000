2b4fd2d60e97f00a8a2b69e05f4c8c19830375b1
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-06-19 04:27:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if !roster[grade]
+      roster[grade] = []
+    end
+    roster[grade] << student
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each do |grade, students|
+      roster[grade] = students.sort
+    end
+    roster
+  end
+
+end
