1f894765a5aebfa7e4c2ea28e59ab6ec739fd34c
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-20 03:08:16 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -9,20 +9,18 @@ class School
   end
 
   def add_student(name, grade)
-    # @roster[grade] = []
-    # @roster[grade].push(name)
-    # @roster[grade].push(name)
-      # binding.pry
-      # if @roster[grade] == nil
-      #   @roster[grade] = []
-      # end
-      @roster[grade] ||= []
+      if @roster[grade] == nil
+         @roster[grade] = []
+      end
+      # @roster[grade] ||= []
       @roster[grade] << name
-        # elsif @roster[grade].include?(name)
-        #   @roster[grade] << name[1]
-
-    # end
   end
 
+  def grade(grade)
+    @roster[grade]
+  end
 
+  def sort
+    @roster.sort.to_h
+  end
 end
