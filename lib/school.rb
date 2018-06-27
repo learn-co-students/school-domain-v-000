5f24adce4e8f583c4505cdb3c8cf56199af08c55
Richard Melchior <raustin.melchior@gmail.com>
2018-06-27 23:54:51 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -6,22 +6,23 @@ class School
   roster = {};
 
   def initialize(name)
-      self.roster = {};
+      @name = name
+      @roster = {};
   end
 
   def add_student(student_name, grade)
-    if !(roster.has_key?(grade))
-      roster[grade] = [];
+    if !@roster[grade]
+      @roster[grade] = [];
     end
     roster[grade] << student_name;
   end
 
   def grade(grade)
-    roster[grade];
+    @roster[grade];
   end
 
   def sort
-    roster.map {|grade, students| roster[grade] = students.sort}
-    roster;
+    @roster.map {|grade, students| @roster[grade] = students.sort}
+    @roster
   end
 end
