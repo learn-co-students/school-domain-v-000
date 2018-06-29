ad990e7d37856a81751a73bb58d8ffcd5d4384bf
Emily Wessel <ettl02@gmail.com>
2018-06-29 00:09:37 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+attr_accessor :name, :roster
+
+def add_student (name, grade)
+  # binding.pry
+
+  if @roster[grade] == nil
+    @roster[grade] = []
+  end
+  @roster[grade] << name
+end
+
+def grade (grade)
+  @roster [grade]
+end
+
+def sort
+  @roster.each do |key, value|
+    value.sort!
+  end
+end
+
+end
