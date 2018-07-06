3b88c7e516352ac689639f7d1a5ff1c599530988
Dana Palmer <roiai4@hotmail.com>
2018-07-06 06:14:05 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
 # code here!
+class School
+  
+ attr_accessor :name
+  
+  def initialize(name, roster)
+    @name = name
+    @roster = Array.new
+  end
+
+  def roster
+    @roster
+  end
+
+end
