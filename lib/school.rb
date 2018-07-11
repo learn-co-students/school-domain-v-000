225b64db0cfc88e18e75603c5ac19e112f05184d
Christine Xiang <yutingcxiang@gmail.com>
2018-07-11 17:49:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+    if @roster.include?(grade) 
+      @roster[grade] << name
+    else 
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.map do |grade, student|
+      student.sort!
+    end
+    @roster
+  end
+
+end
