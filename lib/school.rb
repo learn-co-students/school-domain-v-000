311d8744f9199f6e779202051f676ae580a86436
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-17 22:52:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require "pry"
+class School
+  attr_accessor :name, :roster 
+  
+  
+   def initialize(name)
+   @name = name
+   @roster = {}
+ end
+ 
+ def add_student(student_name, grade)
+ binding.pry
+ roster[grade] ||= []
+ roster[grade] << student_name 
+ 
+ 
+#   if roster[grade] 
+#         roster[grade] << student_name 
+#   else
+#     roster[grade] = []
+#     roster[grade] << student_name 
+# end
+end
+ 
+
+ 
+end
+
+
+  # school = School.new("Bayside High School")
