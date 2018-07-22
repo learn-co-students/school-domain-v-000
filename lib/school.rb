42f7068c4eac9d3e846e39789896b8c65169de07
Anna Dybas <a.m.dybas@gmail.com>
2018-07-22 20:46:58 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,26 @@
-# code here!
 class School
 
-  attr_reader :name, :roster
+  attr_accessor :name, :roster
 
-  def initialize(school_name)
-    @name = school_name
-    roster = {}
+  def initialize(name)
+    @name = name
+    @roster = {}
   end
 
   def add_student(student_name, grade)
     #add students and grade to roster hash, key grade and value student array
-#     hash[new_key] = []
-#     hash[new_key] << new_value_for_value_array
-#
-#     hash
-#      => {"new_key"=>["new_value_for_value_array"]}
-     roster[grade] = []
+     roster[grade] ||= []
      roster[grade] << student_name
+  end
 
+  def grade(grade)
+    roster.values
+  end
+
+  def sort
+    roster.collect do |grade, student|
+      student.sort
+    end
   end
 
 end
