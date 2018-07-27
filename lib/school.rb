1237d8fa9ea40233390128cf0aa2367f24455ef4
tc.thomascho@gmail.com <tc.thomascho@gmail.com>
2018-07-27 17:11:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] = [] if !@roster.keys.include?(grade)
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
+
+end
+
+school = School.new("Bayside High School")
+school.add_student("AC Slater", 10)
