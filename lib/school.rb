dc2ede0c44c44028171ff61e37b29ddc01d490bb
Amee Covarrubias <ameecova@yahoo.com>
2018-07-28 01:11:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+require 'pry'
+class School
+  attr_accessor  :name, :roster
+  attr_reader :add_student, :grade
+  
+  def initialize(school_name)
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+      @roster[grade] ||= []
+      @roster[grade] << student
+  end 
+  
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort 
+    Hash[@roster.sort.map { |a,b| [a, b.sort] }]
+  end 
+end 
