c16608f1c68c8a2dba6b466eeec1fe9ef616919b
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-31 01:57:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require "pry"
+class School
+
+attr_reader :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(name, grade)
+
+
+  if @roster[grade]
+  @roster[grade] << name
+else
+  @roster[grade] = [name]
+  #@roster[grade] << name
+    end
+
+end
+
+#def roster=(key, value)
+#@roster[key] = value
+#end
