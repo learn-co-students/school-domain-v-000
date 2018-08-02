6784f30ef247db4f213e7a1ed46e043126bbeb54
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-02 00:06:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -2,18 +2,14 @@ class School
   attr_accessor :student_name, :grade
   attr_reader :name, :roster
 
-  def initialize(name, roster = {})
+  def initialize(name)
     @name = name
-    @roster = roster
+    @roster = {}
   end
 
   def add_student(student_name, grade)
-    if @roster.keys.include?(grade)
-      @roster[grade] << student_name
-    else
-      @roster[grade] = []
-      @roster[grade] << student_name
-    end
+    @roster ||= grade
+    @roster[grade] << student_name
   end
 
   def grade(grade)
