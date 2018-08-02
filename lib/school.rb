92ff78222e96304809f7b50c9d06c69ec749bfa2
Joann Pan <joannlpan@gmail.com>
2018-08-02 03:03:55 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+
+def initialize(name)
+   @name = name
+end
+
+def roster
+  @roster = {}
+end
+
+def add_student(grade, student)
+  roster[grade] ||= []
+  roster[grade].push(student)
+end
+
+
+end
