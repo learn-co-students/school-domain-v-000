cf71764f28f5b84328638215ddd94141b9aa6358
Mika88 <mikaspivak@gmail.com>
2018-08-02 20:30:06 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+  end
+  def grade(grade)
+    @roster.map do |grades, name|
+      if grade == grades
+         name
+      end
+    end.compact.flatten
+  end
+
+  def sort
+    new = {}
+    @roster.each do |grade, name|
+      new[grade] = name.sort
+    end
+     new
+  end
+end
