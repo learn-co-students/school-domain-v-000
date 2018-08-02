37c153fd6ee5edb667ea9f809ebc44be483cd3e3
Mika88 <mikaspivak@gmail.com>
2018-08-02 20:33:19 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -9,20 +9,16 @@ class School
   end
 
   def add_student(student_name, grade)
-    @roster[grade] ||= []
-    @roster[grade] << student_name
+    roster[grade] ||= []
+    roster[grade] << student_name
   end
   def grade(grade)
-    @roster.map do |grades, name|
-      if grade == grades
-         name
-      end
-    end.compact.flatten
+    roster[grade]
   end
 
   def sort
     new = {}
-    @roster.each do |grade, name|
+    roster.each do |grade, name|
       new[grade] = name.sort
     end
      new
