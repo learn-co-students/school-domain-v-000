b222c638760cc0609dcdd56ad7e41084a54f2974
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-02 00:01:57 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+  attr_accessor :student_name, :grade
+  attr_reader :name, :roster
+
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+  end
+
+  def add_student(student_name, grade)
+    if @roster.keys.include?(grade)
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each {|grade, students| students.sort!}
+  end
+
+end
