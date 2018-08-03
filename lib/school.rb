0748e9bcf416f3fad5844b1986007b612f08fe69
Joann Pan <joannlpan@gmail.com>
2018-08-03 03:04:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,29 @@
+require "pry"
 class School
 
 def initialize(name)
    @name = name
+   @roster = {}
 end
 
 def roster
-  @roster = {}
+  @roster
 end
 
-def add_student(grade, student)
-  roster[grade] ||= []
-  roster[grade].push(student)
+def add_student(student, grade)
+  roster[grade] = [] unless roster[grade]
+  roster[grade] << student
 end
 
+def grade(grade)
+  roster[grade]
+end
 
+def sort
+  sorted = {}
+  roster.each do |grade, students|
+    sorted[grade] = students.sort
+  end
+  sorted
+  end
 end
