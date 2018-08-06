6f9c56c91c3c79583fabf7079fbb6f2674878718
Rafael Lopez <lpz.raf@gmail.com>
2018-08-06 23:06:54 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,5 @@
 # code here!
+require "pry"
 class School
   attr_accessor :name, :roster
   
@@ -14,10 +15,14 @@ class School
   
   def grade(student_grade)
     return @roster[student_grade]
+      
   end
   
-   def sort(student_grade)
-    return @roster[student_grade.sort]
+   def sort
+     new_roster = {}
+     @roster.each do |key, values|
+  new_roster[key] =values.sort
+end
+new_roster
   end
-  
 end
