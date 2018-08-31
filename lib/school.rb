024dd1aa534e2a2d8b4627ef0dad36f530f0f427
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-08-31 04:21:38 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School 
+  attr_accessor :name, :grade, :roster 
+
+  
+  def initialize(roster)
+    @roster = Hash.new 
+    
+  end
+  
+  def add_student(name, grade)
+    #@roster[grade] = []
+    #@roster[grade] << name 
+ 
+
+    if grade == @roster.detect {|n, g| g == grade}
+        @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+
+    
+  end
+  
+end
+
