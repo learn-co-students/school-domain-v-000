9ea17c34ef04da343fe62e0e55765cfd7f0edc56
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2018-08-31 16:58:33 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -11,18 +11,45 @@ class School
   end
   
   def add_student(name, grade)
-    #@roster[grade] = []
+   # @roster[grade] = []
     #@roster[grade] << name
- 
-if grade == @roster.detect {|n, g| g == grade}
-binding.pry
+#binding.pry
+
+  if @roster[grade] == nil 
+    @roster[grade] = []
+  end
+    @roster[grade] << name 
+    
+
+=begin
+    @roster[grade].each do |g, n|
+      if grade == g 
+        @roster[grade] << name
+      else
+        @roster[grade] = []
+        @roster[grade] << name 
+      end
+    end
+
+  if g@rade == @roster.detect {|n, g| g == grade}
       @roster[grade] << name
     else
       @roster[grade] = []
       @roster[grade] << name
     end
-
+=end
     
+  end
+  
+ def grade(grade)
+   @roster[grade].flatten 
+ end
+  
+  def sort
+    sorted = Hash.new
+    @roster.each do |g,n| sorted[g] = n.sort
+  end
+  sorted
   end
   
 end
