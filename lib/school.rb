6a3e8f6a0f631b2ecf23382680ade1b89f778056
Tristan Brown <tgbrown@stanford.edu>
2018-09-02 06:57:21 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+attr_reader :school_name
+
+  def initialize(school_name)
+    @school_name=school_name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name,grade)
+  if roster.include?(grade) == false
+      roster[grade] = []
+    end
+    roster[grade] << name
+  end
+
+def grade(grade)
+  roster[grade]
+end
+
+def sort
+  roster.each do |grade, name|
+  name.sort!
+  end
+end
+end
