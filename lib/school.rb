078c87fd2790ed1038718fb926ac8cd820ce6084
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-09-03 19:12:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+  def initialize(school)
+    @school = school 
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def grade(number)
+    @roster[number]
+  end
+  
+  def add_student(name, grade)
+    @student = name
+    @grade = grade 
+    if @roster.include?(grade) == false 
+      @roster[grade] = []
+    end
+    @roster[grade] << name 
+  end
+  
+  def sort 
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+end
