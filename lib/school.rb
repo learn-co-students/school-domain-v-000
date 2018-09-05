f1626c75c0525c0745f5fa83fc429507e72ec09f
J Fern <jfern20@gmail.com>
2018-09-05 04:54:11 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+class School
+   def initialize(title)
+     @name = title
+     @roster={}
+   end 
+  
+  def roster
+    @roster 
+  end 
+  
+  def add_student(student_name,grade)
+    
+  if @roster.has_key?(grade)
+     @roster[grade].push(student_name)
+  else 
+    @roster[grade] = [student_name]
+  end 
+    end
+  
+   def grade(student_grade)
+     return @roster[student_grade] 
+   end 
+     
+   def sort
+     
+     @roster.each do |key, value|
+    binding 
+  
+  end 
+end 
