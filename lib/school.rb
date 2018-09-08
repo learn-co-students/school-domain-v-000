0147187c17b3a85fe9f55f88265d45b44baa9f46
Ashley Wong <ashwong04@gmail.com>
2018-09-08 20:44:40 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -21,9 +21,14 @@ class School
   end
   
   def grade(grade)
-    
-    
+    @roster[grade]
   end
   
+  def sort
+   @roster.each do |grade, student|
+     @roster[grade] = student.sort
+    end
+    @roster
+  end
   
 end
