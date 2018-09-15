2ae2d868c1e1bbcc0320da7d9bf386beac957c1f
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-15 21:16:35 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    self.roster[grade] = [] unless self.roster.has_key?(grade)
+    self.roster[grade] << student
+  end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+  def sort
+    self.roster.each do |grade, student_array|
+      student_array.sort!
+    end
+  end
+
+end
