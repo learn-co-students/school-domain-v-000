dd30609d06a2bca375307521b3ded51866d287ae
alice T <alicewtien@gmail.com>
2018-09-21 16:38:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if roster[grade] # if the roster already exists, then add names
+      roster[grade] << name
+    else
+      roster[grade] = [] # otherwise create a roster and add names
+      roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    sorted = {} # whenever data is manipulated, always better to created a new hash/array for the new data
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+
+end
