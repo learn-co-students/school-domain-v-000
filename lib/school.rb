80b27d47f251588c96144c2e08bb43a98fd817b9
Stef Claus <stef.claus@gmail.com>
2018-09-25 02:05:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+
+class School 
+  attr_accessor :school_name, :roster 
+
+    
+  def initialize(school_name, roster = {})
+    @school_name = school_name
+    @roster = roster 
+  end 
+end 
+
+
+end #ends class 
+
+#school = School.new("Bayside High School")
+#school.roster
+#school.add_student("Zach Morris", 9)
+#school.roster
