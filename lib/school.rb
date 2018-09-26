39ba7490eb91cc64edbc92d2ace17fd6e7943011
Stef Claus <stef.claus@gmail.com>
2018-09-26 12:56:24 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -3,19 +3,20 @@
 class School 
   attr_accessor :school_name, :roster, :name, :grade
 
-    
   def initialize(school_name, roster = {})
     @school_name = school_name
     @roster = roster 
   end 
 
   
-  def add_student(name, grade)
-      roster[grade]= []
-      roster[grade] << name 
-  end 
   
-
+  def add_student(name, grade)
+    roster[grade] = []
+    if roster[grade] == []
+        roster[grade] << name 
+      else roster[grade] << name 
+   end 
+  end 
 end #ends class 
 
 
