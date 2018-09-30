f51abfed2ebec03f29172b812b94497c23da941c
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-30 14:43:16 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,5 @@
-require 'pry'
-
 class School
-  attr_accessor :roster, :grade, :student_name, :add_student
+  attr_accessor :roster, :grade, :student_name, :add_student, :sort
   attr_reader :name
  
   def initialize(name)
@@ -10,7 +8,16 @@ class School
   end 
   
   def add_student(student_name, grade)
-    @roster[grade] = []
-    @roster[grade] << @student_name
+    @roster[grade] ||= [] 
+    @roster[grade] << student_name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    new_roster = @roster[grade][student_name].values.sort
+    new_roster
   end
 end 
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,4 @@
-gamerequire 'spec_helper'
+require 'spec_helper'
 
 describe 'School' do
   before :each do
