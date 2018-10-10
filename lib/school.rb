8e3a1713eb9623f86fb65737e0ba609d36412a40
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-10 19:03:22 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+  def initialize(school_name)
+    #def roster
+      #roster = {}
+    #end
+    school_name= (school_name)
+    @school_name = school_name
+  end
+  
+  def add_student(student_name, grade)
+  end
+  
+  def grade
+  end
+end
