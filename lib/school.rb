9145cecf4f4c27564bc6fef010d82cd9e0d64a05
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-11 14:04:45 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
+require 'pry'
 # code here!
+
+class School
+  attr_accessor :roster
+
+  def initialize(roster)
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    return @roster[grade]    
+  end
+  
+  def sort
+    @roster.each do |arr ay|
+      # binding.pry
+      array.sort
+      binding.pry
+    end
+  end
+
+end
