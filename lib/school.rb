64f24336c8192ff09f08d3a695a8e7694f1a4e98
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-16 20:20:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -3,6 +3,11 @@ attr_accessor :roster
 
   def initialize(name)
     @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
   end
 
   def name=(name)
@@ -12,4 +17,25 @@ attr_accessor :roster
   def name
     @name
   end
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @grade = grade
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
+
 end
