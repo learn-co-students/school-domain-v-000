b13fe292a11cf793a1873a168103e728557115dd
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2018-10-16 19:45:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,15 @@
-# code here!
+class School
+attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
