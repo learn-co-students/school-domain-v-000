a59f167886a1ed949506ac539140b3aba154c872
Paul Fell-Cinoman <swellpf@gmail.com>
2018-10-19 19:08:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+class School
+  attr_accessor :School, :roster
+  
+  def initialize
+   @roster = {}
+   
+  end
+  
+  def add_student(name, grade)
+    
+  end
+  
+  def grade(grade)
+    
+  end
+  
+end
