298badb90d16b84ffd11700a7e5b480182ed204a
Vince Andrew Pimentel <contact@vincep.me>
2018-10-21 01:19:00 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = Array.new if !@roster.include?(grade)
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster.values_at(grade).flatten
+  end
+
+  def sort
+    @roster.collect { |grade, students| [grade, students.sort] }.to_h
+  end
+end
