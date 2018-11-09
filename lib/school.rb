ac41ac25f462e40766aa69e9f8419abb651f8ea5
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-09 18:24:33 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  attr_accessor :roster, :add_student, :sort
+  
+  def initialize(name)
+    @roster = {}
+  end
+  
+  def name
+    @name = name
+  end
+
+    
+  def add_student(student_name, grade) 
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort 
+    @sort = sort
+  end
+
+end
+
+
