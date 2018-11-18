c7c0c4c4172f05f08f23c70bf2094a495dece7b6
Kimberly Aviles <01010110.v@gmail.com>
2018-11-18 11:43:17 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+
+  ROSTER = {}
+
+  def initialize(school_name)
+    @school_name = school_name
+  end
+
+  def school_name
+    @school_name
+  end
+
+  def roster
+    ROSTER
+  end
+
+  def add_student(student_name, grade)
+
+
+    if ROSTER.include?([grade.to_i])
+      ROSTER[grade.to_i] << [student_name.to_s]
+    else
+      ROSTER[grade.to_i] = []
+      ROSTER[grade.to_i] << student_name.to_s
+    end
+
+  end
+
+
+end # <--- method end
