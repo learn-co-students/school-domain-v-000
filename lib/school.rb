dba91c61674a8ffe8434117d232dfe8fba9afb1e
github username <github email address>
2019-01-30 14:22:26 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,37 +1,55 @@
+require 'pry'
 class School
-  attr_accessor :roster
-  
+  attr_accessor :roster, :add_student, :grade, :sort
+  attr_reader :school
+
+@@all = []
+
   def initialize(school)
     @school = school
     @roster = {}
+    @@all << self
   end
-  
-  def school
-    @school
+  def self.all
+    @@all
   end
-  
-  def roster=(roster) #writer method
-    @roster = roster
+  def add_student(name, grade)
+      @roster[grade] ||= []  #||= will allow this line to run once.
+      @roster[grade] << name
+  end
+  def grade(names_of_students)
+    roster[names_of_students]
+  end
+  def sort
+      sorted = {}
+    roster.each do |grade, students|
+    sorted[grade] ||= students.sort
+  end
+  sorted
   end
 
-  def add_student(name, grade)
-    roster[grade] ||= []
-    roster[grade] << name
-  end
-  
-  def grade(student_grade)
-    roster[student_grade]
-  end
-  
-  def sort
-    sorted = {}
-    roster.each do |grade, students|
-    sorted[grade] = students.sort
-   end
-    sorted
-  end
-  
 end
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+#   def sort
+#     sorted = {}
+#     roster.each do |grade, students|
+#     sorted[grade] = students.sort
+#    end
+#     sorted
+#   end
+#
+# end
