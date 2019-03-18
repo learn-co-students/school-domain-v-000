85bc831b76f7332e11e58320dff2864b02165bf3
Dan Suarez <suarez.daniel101@gmail.com>
2019-03-18 23:16:53 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+#require 'pry'
+class School
+  attr_accessor :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+   def add_student(name, grade)
+     roster[grade] = []
+     roster[grade] << name
+   
+   end
+
+   def grade(grade)
+     roster[grade]
+   end
+end
+#binding.pry
