dbc84c57adadc3687b31338797503e0c11d8366d
ALVIN LU <lualvin1234@gmail.com>
2019-04-08 21:37:21 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ class School
 
   def add_student(student_name, grade)
     @roster[grade] ||=[]
-    @roster[grade]<< student_name
+    @roster[grade] << student_name
   end
 
   def grade(student_grade)
