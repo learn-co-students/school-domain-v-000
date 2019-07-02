f28c7cd1a0ee92dd93f69f1a57b78a9520ad4825
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-07-02 02:08:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,30 +1,34 @@
 # code here!
 class School 
+
+  attr_reader :name
   
   def initialize(name)
     @name = name 
     @roster = {}
-   end
-   
-   def roster 
-     @roster 
-   end 
-   
-   def add_student(name, grade)
-    if @roster[grade] != nil 
-       @roster[grade] << name 
-     else 
-       @roster[grade] = [name] 
-     end
-   end
-   
-   def grade(grade)
-     @roster[grade]
-   end
-   
-   def sort 
-     @roster.each do |key, value|
-       value.sort!
-     end
-   end
+  end
+  
+  def roster 
+    @roster
+  end
+  
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
+
