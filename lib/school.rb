9de2308878d9eef05402ed4cc7af269a18948c87
Richard Hanolt <rhanolt@gmail.com>
2020-05-12 00:49:27 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+  end
+
+  def add_student(student, grade)
+    if roster[grade] != nil
+      roster[grade] << student
+    else
+      roster[grade] = []
+      roster[grade] << student
+    end
+  end
+
+  def grade(grade_number)
+    roster[grade_number]
+  end
+
+end
